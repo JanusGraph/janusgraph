24344e34ea8c6cd6f138d3f6da70a09cf914e99c
Matthias Broecheler <me@matthiasb.com>
2012-06-05 13:40:44 -0700

    Increased block size to 1M again

--- src/main/java/com/thinkaurelius/titan/graphdb/configuration/GraphDatabaseConfiguration.java
+++ src/main/java/com/thinkaurelius/titan/graphdb/configuration/GraphDatabaseConfiguration.java
@@ -89,7 +89,7 @@ public class GraphDatabaseConfiguration {
     public static final String IDAUTHORITY_RETRY_COUNT_KEY = "idauthority-retries";
     public static final int IDAUTHORITY_RETRY_COUNT_DEFAULT = 3;
     public static final String IDAUTHORITY_BLOCK_SIZE_KEY = "idauthority-block-size";
-    public static final int IDAUTHORITY_BLOCK_SIZE_DEFAULT = 10000;
+    public static final int IDAUTHORITY_BLOCK_SIZE_DEFAULT = 100000;
 
 
     public static final String INSTANCE_RID_RAW_KEY = "machine-id";
