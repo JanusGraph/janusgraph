018a46863d53500212f97dabff49ea945e04975b
Marko Rodriguez <okrammarko@gmail.com>
2012-06-06 08:38:01 -0600

    pushed Gremlin for Windows, made Gremlin dependency </provided>, removed package.html files, removed javadoc.xml. tweaks to distribution.xml.

---
+++ gremlin.bat
@@ -0,0 +1,13 @@
+:: Windows launcher script for Gremlin
+@echo off
+
+cd %CD%\target\
+
+set TARGET=
+
+for /f "tokens=*" %%a in ('dir /b /ad') do (
+if exist "%%a\bin\gremlin.bat" set TARGET=%%a
+)
+
+cd %TARGET%\bin\
+call gremlin.bat %*
