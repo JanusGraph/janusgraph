8f2a66232feb73a7908b7f70a7c68306bf10bf02
Matthias Broecheler <me@matthiasb.com>
2012-06-11 16:11:52 -0700

    Disabled the memory assertion in the test case because it depends too much on the JVM running the test and might fail if GC is not invoked properly.

--- src/test/java/com/thinkaurelius/titan/graphdb/TitanGraphTest.java
+++ src/test/java/com/thinkaurelius/titan/graphdb/TitanGraphTest.java
@@ -45,7 +45,7 @@ public abstract class TitanGraphTest extends TitanGraphTestCommon {
         close();
         long memoryAfter = MemoryAssess.getMemoryUse();
         log.debug("Memory after: {}",memoryAfter/1024);
-        assertTrue(memoryAfter<100*1024*1024);
+        //assertTrue(memoryAfter<100*1024*1024);
     }
     
     @Test
