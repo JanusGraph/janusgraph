4ceaa101601ea88dd089f780cd944509d00da161
Marko Rodriguez <okrammarko@gmail.com>
2012-07-09 18:04:22 -0600

    the first push of the Faunus DSL which is basically Gremlin--

---
+++ src/main/java/com/thinkaurelius/faunus/drivers/Faunus.java
@@ -0,0 +1,124 @@
+package com.thinkaurelius.faunus.drivers;
+
+import com.thinkaurelius.faunus.io.formats.json.JSONInputFormat;
+import com.thinkaurelius.faunus.io.formats.json.JSONOutputFormat;
+import com.thinkaurelius.faunus.io.graph.FaunusVertex;
+import com.thinkaurelius.faunus.mapreduce.algebra.Identity;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.InputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.OutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.codehaus.groovy.jsr223.GroovyScriptEngineImpl;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
+ */
+public class Faunus extends Configured implements Tool {
+
+    private final Class<? extends InputFormat> inputFormat;
+    private final Class<? extends OutputFormat> outputFormat;
+    private final Path outputPath;
+    private Path inputPath;
+
+    private List<Job> jobs = new ArrayList<Job>();
+
+    public Faunus(Class<? extends InputFormat> inputFormat, Path inputPath, Class<? extends OutputFormat> outputFormat, Path outputPath) {
+        this.inputFormat = inputFormat;
+        this.outputFormat = outputFormat;
+        this.outputPath = outputPath;
+        this.inputPath = inputPath;
+        this.setConf(new Configuration());
+    }
+
+    public Faunus _() throws IOException {
+        final Job job = new Job(this.getConf(), "Faunus: " + Identity.class.getSimpleName());
+        job.setJarByClass(Faunus.class);
+        job.setMapperClass(Identity.Map.class);
+        job.setMapOutputKeyClass(NullWritable.class);
+        job.setMapOutputValueClass(FaunusVertex.class);
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(FaunusVertex.class);
+
+        this.jobs.add(job);
+        return this;
+    }
+
+    public int run(String[] args) throws Exception {
+
+        this.composeJobs();
+        for (final Job job : this.jobs) {
+            job.waitForCompletion(true);
+            FileSystem hdfs = FileSystem.get(job.getConfiguration());
+            String tempFile = job.getConfiguration().get("inputPath");
+            if (null != tempFile)
+                hdfs.delete(new Path(tempFile), true);
+        }
+
+        return 0;
+    }
+
+    private void composeJobs() throws IOException {
+        if (this.jobs.size() == 0) {
+            return;
+        }
+
+        String uuid = UUID.randomUUID().toString();
+        final Job startJob = this.jobs.get(0);
+        startJob.setInputFormatClass(this.inputFormat);
+
+        FileInputFormat.setInputPaths(startJob, this.inputPath);
+        if (this.jobs.size() > 1) {
+            FileOutputFormat.setOutputPath(startJob, new Path(uuid));
+            startJob.setOutputFormatClass(SequenceFileOutputFormat.class);
+        } else {
+            FileOutputFormat.setOutputPath(startJob, this.outputPath);
+            startJob.setOutputFormatClass(this.outputFormat);
+
+        }
+
+        if (this.jobs.size() > 2) {
+            for (int i = 1; i < this.jobs.size() - 1; i++) {
+                final Job midJob = this.jobs.get(i);
+                midJob.setInputFormatClass(SequenceFileInputFormat.class);
+                midJob.setOutputFormatClass(SequenceFileOutputFormat.class);
+                FileInputFormat.setInputPaths(midJob, new Path(uuid));
+                midJob.getConfiguration().set("inputPath", uuid);
+                uuid = UUID.randomUUID().toString();
+                FileOutputFormat.setOutputPath(midJob, new Path(uuid));
+            }
+        }
+        if (this.jobs.size() > 1) {
+            final Job endJob = this.jobs.get(this.jobs.size() - 1);
+            endJob.setInputFormatClass(SequenceFileInputFormat.class);
+            endJob.setOutputFormatClass(this.outputFormat);
+            endJob.getConfiguration().set("inputPath", uuid);
+            FileInputFormat.setInputPaths(endJob, new Path(uuid));
+            FileOutputFormat.setOutputPath(endJob, this.outputPath);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        final Faunus faunus = new Faunus(JSONInputFormat.class, new Path(args[0]), JSONOutputFormat.class, new Path(args[1]));
+        final GroovyScriptEngineImpl scriptEngine = new GroovyScriptEngineImpl();
+        scriptEngine.put("V", faunus);
+        scriptEngine.eval(args[2]);
+        int result = ToolRunner.run(faunus, args);
+        System.exit(result);
+    }
+
+}
