8341bbf61a64fc004e5e1838c60889d6835941a9
Marko Rodriguez <okrammarko@gmail.com>
2012-07-20 13:59:58 -0600

    tweaks for EC2.

--- bin/faunus.sh
+++ bin/faunus.sh
@@ -7,13 +7,7 @@ else
     HADOOP="$HADOOP_HOME/bin/hadoop"
 fi
 
-# Read command line parameters
-input=$1
-output=$2
-overwrite=$3
-script=$4
-
 # Deploy job to Hadoop cluster
-$HADOOP jar target/faunus-*-job.jar com.thinkaurelius.faunus.FaunusGraph $1 $2 $3 $4
+$HADOOP jar target/faunus-*-job.jar com.thinkaurelius.faunus.FaunusGraph $1 $2
 
 exit $?
