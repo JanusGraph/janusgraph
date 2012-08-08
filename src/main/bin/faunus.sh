ef3fb3c50ee30940eb9d36c27daba33b2c02446c
Marko Rodriguez <okrammarko@gmail.com>
2012-08-08 12:32:34 -0600

    worked out the distribution.xml.

---
+++ src/main/bin/faunus.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Find Hadoop
+if [ "$HADOOP_HOME" = "" ] ; then
+    HADOOP="hadoop"
+else
+    HADOOP="$HADOOP_HOME/bin/hadoop"
+fi
+
+# Deploy job to Hadoop cluster
+$HADOOP jar lib/faunus-*-job.jar com.thinkaurelius.faunus.FaunusGraph $1 $2
+
+exit $?
