4de3f7ffc670f5068610bd41aaa955c6d6d18f9a
stephen mallette <spmva@genoprime.com>
2012-09-18 12:46:25 -0400

    basic .bat for faunus.

---
+++ src/main/bin/gremlin.bat
@@ -0,0 +1,54 @@
+@ECHO ON
+setlocal enabledelayedexpansion
+
+SET LIBDIR=..\lib
+SET CP=%LIBDIR%/*
+
+:: find hadoop
+IF "%HADOOP_CONF_DIR%" NEQ "" (
+  SET CP=%CP%;%HADOOP_CONF_DIR%
+) ELSE (
+  IF "%HADOOP_CONF%" NEQ "" (
+    SET CP=%CP%;%HADOOP_CONF%
+  ) ELSE (
+    SET CP=%CP%;%HADOOP_HOME%/conf
+  )
+)
+
+IF "%JAVA_OPTIONS%" EQU "" (
+  SET JAVA_OPTIONS=-Xms32m -Xmx512m
+)
+
+SET K=
+
+IF "%1" EQU "-e" (
+  FOR %%A IN (%*) DO (
+    IF "%%A" NEQ "-e" (
+      SET K=%%A
+    )
+  )
+
+  java %JAVA_OPTIONS% -cp %CP%;%CLASSPATH% com.thinkaurelius.faunus.tinkerpop.gremlin.ScriptExecutor !K!
+  GOTO done
+)
+
+IF "%1" EQU "-i" (
+  FOR %%A IN (%*) DO (
+    IF "%%A" NEQ "-e" (
+      SET K=%%A
+    )
+  )
+
+  java %JAVA_OPTIONS% -cp %CP%;%CLASSPATH% com.thinkaurelius.faunus.tinkerpop.gremlin.InlineScriptExecutor !K!
+  GOTO done
+)
+
+IF "%1" EQU "-v" (
+  java %JAVA_OPTIONS% -cp %CP%;%CLASSPATH% com.thinkaurelius.faunus.tinkerpop.gremlin.Version
+) ELSE (
+  java %JAVA_OPTIONS% -cp %CP%;%CLASSPATH% com.thinkaurelius.faunus.tinkerpop.gremlin.Console
+)
+
+GOTO done
+
+:done
