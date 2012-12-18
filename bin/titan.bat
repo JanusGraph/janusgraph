5185da263358ad823fd3014fda483c9f10c30cb7
Stephen Mallette <spmva@genoprime.com>
2012-12-18 14:12:50 -0500

    Basic .bat files to start titan.

---
+++ bin/titan.bat
@@ -0,0 +1,13 @@
+:: Windows launcher script for Titan
+@echo off
+
+cd %CD%\..\target\
+
+set TARGET=
+
+for /f "tokens=*" %%a in ('dir /b /ad') do (
+if exist "%%a\bin\titan.bat" set TARGET=%%a
+)
+
+cd %TARGET%\bin\
+call titan.bat %*
