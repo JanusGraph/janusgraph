478673b0e274215458b97db4c645581f7236557f
okram <okrammarko@gmail.com>
2013-01-19 17:00:44 -0700

    Removed 0.1.0 wiki from docs.

--- src/main/java/com/thinkaurelius/faunus/formats/MapReduceFormat.java
+++ src/main/java/com/thinkaurelius/faunus/formats/MapReduceFormat.java
@@ -5,6 +5,10 @@ import com.thinkaurelius.faunus.mapreduce.FaunusCompiler;
 import java.io.IOException;
 
 /**
+ * If an Input- or OutputFormat requires some pre-post processing, then a MapReduceFormat can be implemented.
+ * For InputFormats, the MapReduce jobs are prepended to the job pipeline and the final job must yield a FaunusVertex stream.
+ * For OutputFormats, the MapReduce jobs are appended to the job pipeline and the first job must consume a FaunusVertex stream.
+ *
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
 public interface MapReduceFormat {
