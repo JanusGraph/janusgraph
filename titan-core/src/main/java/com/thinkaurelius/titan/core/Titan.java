b8e886059c4d62303bf2298ec4d0ed7e0c4fa71c
Pavel Yaskevich <xedin@apache.org>
2013-01-21 13:33:57 -0800

    add Titan.version()

---
+++ titan-core/src/main/java/com/thinkaurelius/titan/core/Titan.java
@@ -0,0 +1,9 @@
+package com.thinkaurelius.titan.core;
+
+import com.thinkaurelius.titan.graphdb.configuration.TitanConstants;
+
+public class Titan {
+    public static String version() {
+        return TitanConstants.VERSION;
+    }
+}
