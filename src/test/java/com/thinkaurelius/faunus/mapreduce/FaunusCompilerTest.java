3194e5412f2e04847a311762d4168c9c9388200f
Marko A. Rodriguez <okrammarko@gmail.com>
2013-03-11 19:23:44 -0600

    Added more test cases to FaunusCompilerTest.

--- src/test/java/com/thinkaurelius/faunus/mapreduce/FaunusCompilerTest.java
+++ src/test/java/com/thinkaurelius/faunus/mapreduce/FaunusCompilerTest.java
@@ -3,6 +3,7 @@ package com.thinkaurelius.faunus.mapreduce;
 import com.thinkaurelius.faunus.FaunusGraph;
 import com.thinkaurelius.faunus.FaunusVertex;
 import com.thinkaurelius.faunus.mapreduce.transform.IdentityMap;
+import com.thinkaurelius.faunus.mapreduce.util.CountMapReduce;
 import junit.framework.TestCase;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.NullWritable;
@@ -19,6 +20,30 @@ public class FaunusCompilerTest extends TestCase {
         compiler.addMap(IdentityMap.Map.class, NullWritable.class, FaunusVertex.class, new Configuration());
         compiler.completeSequence();
         assertEquals(compiler.jobs.get(0).getConfiguration().getInt("a_property", -1), 2);
+        assertEquals(compiler.jobs.get(0).getConfiguration().getInt("b_property", -1), -1);
+    }
 
+    public void testJobListSize() {
+        FaunusCompiler compiler = new FaunusCompiler(new FaunusGraph());
+        assertEquals(compiler.jobs.size(), 0);
+        compiler.addMap(IdentityMap.Map.class, NullWritable.class, FaunusVertex.class, new Configuration());
+        assertEquals(compiler.jobs.size(), 0);
+        compiler.addMapReduce(CountMapReduce.Map.class, null, CountMapReduce.Reduce.class, NullWritable.class, FaunusVertex.class, NullWritable.class, FaunusVertex.class, new Configuration());
+        assertEquals(compiler.jobs.size(), 1);
+        compiler.addMapReduce(CountMapReduce.Map.class, null, CountMapReduce.Reduce.class, NullWritable.class, FaunusVertex.class, NullWritable.class, FaunusVertex.class, new Configuration());
+        assertEquals(compiler.jobs.size(), 2);
+    }
+
+    public void testJobOrder() throws Exception {
+        FaunusCompiler compiler = new FaunusCompiler(new FaunusGraph());
+        assertEquals(compiler.jobs.size(), 0);
+        compiler.addMap(IdentityMap.Map.class, NullWritable.class, FaunusVertex.class, new Configuration());
+        assertEquals(compiler.jobs.size(), 0);
+        compiler.addMapReduce(CountMapReduce.Map.class, null, CountMapReduce.Reduce.class, NullWritable.class, FaunusVertex.class, NullWritable.class, FaunusVertex.class, new Configuration());
+        assertEquals(compiler.jobs.size(), 1);
+
+        assertEquals(compiler.jobs.get(0).getMapperClass(), MapSequence.Map.class);
+        assertEquals(compiler.jobs.get(0).getCombinerClass(), null);
+        assertEquals(compiler.jobs.get(0).getReducerClass(), CountMapReduce.Reduce.class);
     }
 }
