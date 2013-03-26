81daf1db36477b171534268815cc60ad1abf8610
Stephen Mallette <spmva@genoprime.com>
2013-03-26 18:19:26 -0400

    Initialize Gremlin repl with a groovy script. #132

--- titan-core/src/main/java/com/thinkaurelius/titan/tinkerpop/gremlin/Console.java
+++ titan-core/src/main/java/com/thinkaurelius/titan/tinkerpop/gremlin/Console.java
@@ -10,13 +10,19 @@ import org.codehaus.groovy.tools.shell.Groovysh;
 import org.codehaus.groovy.tools.shell.IO;
 import org.codehaus.groovy.tools.shell.InteractiveShellRunner;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 /**
  * The Gremlin console adapted for the Titan Graph Database
  *
  * @author Marko A. Rodriguez (http://markorodriguez.com)
+ * @author Stephen Mallette (http://stephen.genoprime.com)
  */
 public class Console {
 
@@ -24,7 +30,7 @@ public class Console {
     private static final String STANDARD_INPUT_PROMPT = "gremlin> ";
     private static final String STANDARD_RESULT_PROMPT = "==>";
 
-    public Console(final IO io, final String inputPrompt, final String resultPrompt) {
+    public Console(final IO io, final String inputPrompt, final String resultPrompt, final String initScriptFile) {
         io.out.println();
         io.out.println("         \\,,,/");
         io.out.println("         (o o)");
@@ -58,6 +64,8 @@ public class Console {
         }
 
         Gremlin.load();
+        initializeShellWithScript(io, initScriptFile, groovy);
+
         try {
             runner.run();
         } catch (Error e) {
@@ -66,11 +74,38 @@ public class Console {
     }
 
     public Console() {
-        this(new IO(System.in, System.out, System.err), STANDARD_INPUT_PROMPT, STANDARD_RESULT_PROMPT);
+        // opted to keep this constructor after adding the one with the initialization script file in case something
+        // is using it...not sure if anything is.  if not, it can go.
+        this(null);
+    }
+
+    public Console(final String initScriptFile) {
+        this(new IO(System.in, System.out, System.err), STANDARD_INPUT_PROMPT, STANDARD_RESULT_PROMPT, initScriptFile);
+    }
+
+    private void initializeShellWithScript(final IO io, final String initScriptFile, final Groovysh groovy) {
+        if (initScriptFile != null) {
+            String line = "";
+            try {
+                final BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        new FileInputStream(initScriptFile), Charset.forName("UTF-8")));
+                while ((line = reader.readLine()) != null) {
+                    groovy.execute(line);
+                }
+
+                reader.close();
+            } catch (FileNotFoundException fnfe) {
+                io.err.println(String.format("Gremlin initialization file not found at [%s].", initScriptFile));
+                System.exit(1);
+            } catch (IOException ioe) {
+                io.err.println(String.format("Bad line in Gremlin initialization file at [%s].", line));
+                System.exit(1);
+            }
+        }
     }
 
 
     public static void main(final String[] args) {
-        new Console();
+        new Console(args.length == 1 ? args[0] : null);
     }
 }
