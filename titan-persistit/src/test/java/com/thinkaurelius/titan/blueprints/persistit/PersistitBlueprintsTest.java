d8c57570a1fdf50061db11d04567db3ba8b5f6b0
Blake Eggleston <bdeggleston@gmail.com>
2013-03-30 14:58:36 -0700

    moving tests into persistit package

---
+++ titan-persistit/src/test/java/com/thinkaurelius/titan/blueprints/persistit/PersistitBlueprintsTest.java
@@ -0,0 +1,7 @@
+package com.thinkaurelius.titan.blueprints.persistit;
+
+import com.thinkaurelius.titan.PersistitStorageSetup;
+import com.thinkaurelius.titan.blueprints.TitanBlueprintsTest;
+
+//public class PersistitBlueprintsTest extends TitanBlueprintsTest {
+//}
