61b84690432bea2ace49cc080cef40b7e2ac9762
Stephen Mallette <spmva@genoprime.com>
2013-05-06 15:38:22 -0400

    Add ext directory to drop extensions for titan server into the classpath.

--- titan-all/src/main/bin/titan.bat
+++ titan-all/src/main/bin/titan.bat
@@ -3,6 +3,15 @@
 @echo off
 
 set LIBDIR=..\lib
+set EXTDIR=ext/*
+
+cd ext
+
+FOR /D /r %%i in (*) do (
+    set EXTDIR=%EXTDIR%;%%i/*
+)
+
+cd ..
 
 set OLD_CLASSPATH=%CLASSPATH%
 set CP=
@@ -15,4 +24,4 @@ set JAVA_OPTIONS=-Xms32m^
 
 :: Launch the application
 
-java %JAVA_OPTIONS% %JAVA_ARGS% -cp %LIBDIR%/*; com.thinkaurelius.titan.tinkerpop.rexster.RexsterTitanServer %*
+java %JAVA_OPTIONS% %JAVA_ARGS% -cp %LIBDIR%/*;%EXTDIR%; com.thinkaurelius.titan.tinkerpop.rexster.RexsterTitanServer %*
