e1ebf1275a27dbec79f44960b4786c7696ebb8d3
Dan LaRocque <dalaro@hopcount.org>
2013-05-09 16:40:59 -0400

    Untested refactoring of LocalLockMediatorTest
    Replaced ByteBuffer with StaticBuffer.  The changes required in this
    short test file touch only two lines and seem completely trivial.

--- titan-test/src/test/java/com/thinkaurelius/titan/diskstorage/locking/LocalLockMediatorTest.java
+++ titan-test/src/test/java/com/thinkaurelius/titan/diskstorage/locking/LocalLockMediatorTest.java
@@ -1,8 +1,11 @@
 package com.thinkaurelius.titan.diskstorage.locking;
 
+import com.thinkaurelius.titan.diskstorage.StaticBuffer;
 import com.thinkaurelius.titan.diskstorage.locking.consistentkey.ConsistentKeyLockTransaction;
 import com.thinkaurelius.titan.diskstorage.locking.consistentkey.LocalLockMediator;
 import com.thinkaurelius.titan.diskstorage.util.KeyColumn;
+import com.thinkaurelius.titan.diskstorage.util.StaticByteBuffer;
+
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
@@ -14,8 +17,8 @@ import static org.mockito.Mockito.mock;
 public class LocalLockMediatorTest {
 
     private static final String LOCK_NAMESPACE = "test";
-    private static final ByteBuffer LOCK_ROW = ByteBuffer.wrap(new byte[]{1});
-    private static final ByteBuffer LOCK_COL = ByteBuffer.wrap(new byte[]{1});
+    private static final StaticBuffer LOCK_ROW = new StaticByteBuffer(ByteBuffer.wrap(new byte[]{1}));
+    private static final StaticBuffer LOCK_COL = new StaticByteBuffer(ByteBuffer.wrap(new byte[]{1}));
     private static final KeyColumn kc = new KeyColumn(LOCK_ROW, LOCK_COL);
     //	private static final long LOCK_EXPIRATION_TIME_MS = 1;
 //	private static final long SLEEP_MS = LOCK_EXPIRATION_TIME_MS * 1000;
