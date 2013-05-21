fb4b10cdb80b7b2f27e4d87e9469469de0a99a5a
Marko A. Rodriguez <okrammarko@gmail.com>
2013-05-21 11:52:04 -0600

    Faunus 0.3.1 release commit.

--- src/main/java/com/thinkaurelius/faunus/Tokens.java
+++ src/main/java/com/thinkaurelius/faunus/Tokens.java
@@ -5,7 +5,7 @@ package com.thinkaurelius.faunus;
  */
 public class Tokens {
 
-    public static final String VERSION = "0.3.1-SNAPSHOT";
+    public static final String VERSION = "0.3.1";
 
     public enum Action {DROP, KEEP}
 
