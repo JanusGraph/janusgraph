JanusGraph/janusgraph

commit 37b84fb6425485bd5f2ab0396885a9a9d616e47c

Remove unchecked cast in AbstractElement#equals()
Modify AbstractElement's equals() method to eliminate potential
unchecked casts while attempting to stay as close as possible to the
behavior of the original implementation.

closes #286

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/internal/AbstractElement.java
@@ -33,12 +33,17 @@ public abstract class AbstractElement implements InternalElement {
 
     @Override
     public boolean equals(Object other) {
-        if (this==other) return true;
-        else if (other==null) return false;
-        if (((this instanceof TitanVertex) && (other instanceof TitanVertex)) ||
-                ((this instanceof TitanEdge) && (other instanceof TitanEdge)))
-            return getID()==((AbstractElement)other).getID();
-        else return false;
+        if (this==other) {
+            return true;
+        } else if (other==null) {
+            return false;
+        } else if (this instanceof TitanVertex && other instanceof TitanVertex) {
+            return getId().equals(((TitanVertex)other).getId());
+        } else if (this instanceof TitanEdge && other instanceof TitanEdge) {
+            return getId().equals(((TitanEdge)other).getId());
+        } else {
+            return false;
+        }
     }
 
 

commit 162f942f4839b451fac706a2b0fa49f2eaf5ae77

Call AbstractElement getID() insteado of getId()
Relying on capitalization significance to differentiate methods on
this object seems like asking for trouble.  For now, just fix the
capitalization bug introduced by
37b84fb6425485bd5f2ab0396885a9a9d616e47c, but we might want to
refactor getID() and getId() to more distinct names.

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/internal/AbstractElement.java
@@ -38,9 +38,9 @@ public abstract class AbstractElement implements InternalElement {
         } else if (other==null) {
             return false;
         } else if (this instanceof TitanVertex && other instanceof TitanVertex) {
-            return getId().equals(((TitanVertex)other).getId());
+            return getID() == ((TitanVertex)other).getID();
         } else if (this instanceof TitanEdge && other instanceof TitanEdge) {
-            return getId().equals(((TitanEdge)other).getId());
+            return getID() == ((TitanEdge)other).getID();
         } else {
             return false;
         }