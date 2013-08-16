209128c31a7f4363828bd67c2cd57c9fc2daf838
Dan LaRocque <dalaro@hopcount.org>
2013-08-16 10:40:03 -0400

    Moved package tools under maven control
    The debian, redhat, and pkgcommon directories no longer live in the
    root of the repository.  Instead, they live under
    titan-dist/titan-dist-all/src/pkg.  Running "mvn install -Ppkg-tools"
    in titan-dist-all will copy the directories into the root.  One file
    is currently filtered: pkgcommon/etc/version.sh.  This file contains
    Titan's Maven version information; in future commits, I'm going to
    update the package build toolchain to use this version information
    instead of hardcoding it.

---
+++ titan-dist/titan-dist-all/src/pkg/resources/version.sh
@@ -0,0 +1,5 @@
+declare -r VER_MAJOR=${parsedVersion.majorVersion}
+declare -r VER_MINOR=${parsedVersion.minorVersion}
+declare -r VER_PATCH=${parsedVersion.incrementalVersion}
+declare -r VER_QUALIFIER=${parsedVersion.qualifier}
+declare -r VER_BUILDNUMBER=${parsedVersion.buildNumber}
---
+++ titan-dist/titan-dist-all/src/pkg/static/pkgcommon/bin/update-local-repos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+set -u
+
+.  "`dirname $0`"/../config.sh.in
+cd "`dirname $0`"/../
+
