a8ad2ba03a81a8150f662929083683313e4f3d2d
Matthias Broecheler <me@matthiasb.com>
2013-09-07 16:32:22 -0700

    Fixes #360

--- titan-core/src/main/java/com/thinkaurelius/titan/core/attribute/FullDouble.java
+++ titan-core/src/main/java/com/thinkaurelius/titan/core/attribute/FullDouble.java
@@ -2,11 +2,11 @@ package com.thinkaurelius.titan.core.attribute;
 
 /**
  * Helper class to be used when wanting to store the full value spectrum of a double value.
- *
+ * <p/>
  * By default, Titan will convert double values into a decimal format so that it can be stored in byte-order
  * and be used for vertex-centric indices. However, that restricts the value spectrum of doubles when registered
  * using {@link com.thinkaurelius.titan.core.TypeMaker#dataType(Class)} with Double.class as argument.
- *
+ * <p/>
  * When order preservation is not important, use FullDouble as the data type.
  *
  * @author Matthias Broecheler (me@matthiasb.com)
@@ -16,30 +16,49 @@ public class FullDouble extends Number {
 
     private double value;
 
-    private FullDouble() {}
+    private FullDouble() {
+    }
 
     public FullDouble(final double value) {
-        this.value=value;
+        this.value = value;
     }
 
     @Override
     public int intValue() {
-        return (int)value;
+        return (int) value;
     }
 
     @Override
     public long longValue() {
-        return (long)value;
+        return (long) value;
     }
 
     @Override
     public float floatValue() {
-        return (float)value;
+        return (float) value;
     }
 
     @Override
     public double doubleValue() {
         return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Double.valueOf(value).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(value);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) return true;
+        else if (other == null) return false;
+        else if (!(other instanceof Number)) return false;
+        else return value == ((Number) other).doubleValue();
     }
 
 }
--- titan-core/src/main/java/com/thinkaurelius/titan/core/attribute/FullFloat.java
+++ titan-core/src/main/java/com/thinkaurelius/titan/core/attribute/FullFloat.java
@@ -2,11 +2,11 @@ package com.thinkaurelius.titan.core.attribute;
 
 /**
  * Helper class to be used when wanting to store the full value spectrum of a float value.
- *
+ * <p/>
  * By default, Titan will convert float values into a decimal format so that it can be stored in byte-order
  * and be used for vertex-centric indices. However, that restricts the value spectrum of floats when registered
  * using {@link com.thinkaurelius.titan.core.TypeMaker#dataType(Class)} with Float.class as argument.
- *
+ * <p/>
  * When order preservation is not important, use FullFloat as the data type.
  *
  * @author Matthias Broecheler (me@matthiasb.com)
@@ -16,20 +16,21 @@ public class FullFloat extends Number {
 
     private float value;
 
-    private FullFloat() {}
+    private FullFloat() {
+    }
 
     public FullFloat(final float value) {
-        this.value=value;
+        this.value = value;
     }
 
     @Override
     public int intValue() {
-        return (int)value;
+        return (int) value;
     }
 
     @Override
     public long longValue() {
-        return (long)value;
+        return (long) value;
     }
 
     @Override
@@ -40,6 +41,24 @@ public class FullFloat extends Number {
     @Override
     public double doubleValue() {
         return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Float.floatToIntBits(value);
+    }
+
+    @Override
+    public String toString() {
+        return Float.toString(value);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) return true;
+        else if (other == null) return false;
+        else if (!(other instanceof Number)) return false;
+        else return value == ((Number) other).floatValue();
     }
 
 }
