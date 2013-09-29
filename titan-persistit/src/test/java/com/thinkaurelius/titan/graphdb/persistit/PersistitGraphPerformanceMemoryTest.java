493e45ce8b5dfe05731fcd7d055c008652c87637
Dan LaRocque <dalaro@hopcount.org>
2013-09-29 10:08:23 -0400

    Tune MemoryTests footprint
    Increased heap memory from 128 to 512 MB after producing OOME and GC
    overhead limit exceeded on Persistit with 128.  Reducing the Persistit
    buffercount to 2048 (x 16384 bytes) @ 128 MB heap still wasn't enough
    to avoid the errors.

--- titan-persistit/src/test/java/com/thinkaurelius/titan/graphdb/persistit/PersistitGraphPerformanceMemoryTest.java
+++ titan-persistit/src/test/java/com/thinkaurelius/titan/graphdb/persistit/PersistitGraphPerformanceMemoryTest.java
@@ -1,11 +1,21 @@
 package com.thinkaurelius.titan.graphdb.persistit;
 
+import org.apache.commons.configuration.Configuration;
+
 import com.thinkaurelius.titan.PersistitStorageSetup;
+import com.thinkaurelius.titan.diskstorage.persistit.PersistitStoreManager;
 import com.thinkaurelius.titan.graphdb.TitanGraphPerformanceMemoryTest;
+import com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration;
 
 public class PersistitGraphPerformanceMemoryTest extends TitanGraphPerformanceMemoryTest {
 
     public PersistitGraphPerformanceMemoryTest() {
-        super(PersistitStorageSetup.getPersistitGraphConfig());
+        super(getGraphConfig());
+    }
+    
+    private static Configuration getGraphConfig() {
+        Configuration base = PersistitStorageSetup.getPersistitGraphConfig();
+        base.subset(GraphDatabaseConfiguration.STORAGE_NAMESPACE).setProperty("buffercount", 1024);
+        return base;
     }
 }
