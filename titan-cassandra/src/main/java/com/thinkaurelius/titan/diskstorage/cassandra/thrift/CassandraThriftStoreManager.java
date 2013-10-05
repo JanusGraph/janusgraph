492648a8cf1809e3509e569d7886121238958c10
Dan LaRocque <dalaro@hopcount.org>
2013-10-05 07:39:41 -0400

    Logging tweaks in Cassandra's clearStorage()

--- titan-cassandra/src/main/java/com/thinkaurelius/titan/diskstorage/cassandra/thrift/CassandraThriftStoreManager.java
+++ titan-cassandra/src/main/java/com/thinkaurelius/titan/diskstorage/cassandra/thrift/CassandraThriftStoreManager.java
@@ -247,6 +247,13 @@ public class CassandraThriftStoreManager extends AbstractCassandraStoreManager {
      */
     public void clearStorage() throws StorageException {
         openStores.clear();
+        final String lp = "ClearStorage: "; // "log prefix"
+        /*
+         * log4j is capable of automatically writing the name of a method that
+         * generated a log message, but the docs warn that "generating caller
+         * location information is extremely slow and should be avoided unless
+         * execution speed is not an issue."
+         */
 
         CTConnection conn = null;
         try {
@@ -258,29 +265,29 @@ public class CassandraThriftStoreManager extends AbstractCassandraStoreManager {
                 client.set_keyspace(keySpaceName);
                 ksDef = client.describe_keyspace(keySpaceName);
             } catch (NotFoundException e) {
-                log.error("Keyspace {} does not exist, not attempting to truncate.", keySpaceName);
+                log.debug(lp + "Keyspace {} does not exist, not attempting to truncate.", keySpaceName);
                 return;
             } catch (InvalidRequestException e) {
-                log.error("InvalidRequestException when attempting to describe keyspace {}, not attempting to truncate.", keySpaceName);
+                log.debug(lp + "InvalidRequestException when attempting to describe keyspace {}, not attempting to truncate.", keySpaceName);
                 return;
             }
 
 
             if (null == ksDef) {
-                log.debug("Received null KsDef for keyspace {}; not truncating its CFs", keySpaceName);
+                log.debug(lp + "Received null KsDef for keyspace {}; not truncating its CFs", keySpaceName);
                 return;
             }
 
             List<CfDef> cfDefs = ksDef.getCf_defs();
 
             if (null == cfDefs) {
-                log.debug("Received empty CfDef list for keyspace {}; not truncating CFs", keySpaceName);
+                log.debug(lp + "Received empty CfDef list for keyspace {}; not truncating CFs", keySpaceName);
                 return;
             }
 
             for (CfDef cfDef : ksDef.getCf_defs()) {
                 client.truncate(cfDef.name);
-                log.info("Truncated CF {} in keyspace {}", cfDef.name, keySpaceName);
+                log.info(lp + "Truncated CF {} in keyspace {}", cfDef.name, keySpaceName);
             }
                 
             /*
