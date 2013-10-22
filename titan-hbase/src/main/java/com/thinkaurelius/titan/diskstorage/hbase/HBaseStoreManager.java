8a865880c1b7e310fd02d593a45407b8a17843b4
Dan LaRocque <dalaro@hopcount.org>
2013-10-22 11:31:21 -0400

    Added option to shorten HBase CF names
    Per advice in http://hbase.apache.org/book/rowkey.design.html#keysize.cf
    
    This option's configuration property key is storage.short-cf-names.
    It's honored only by the HBase store manager.  It's disabled by
    default.

--- titan-hbase/src/main/java/com/thinkaurelius/titan/diskstorage/hbase/HBaseStoreManager.java
+++ titan-hbase/src/main/java/com/thinkaurelius/titan/diskstorage/hbase/HBaseStoreManager.java
@@ -26,6 +26,11 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+import static com.thinkaurelius.titan.diskstorage.Backend.EDGESTORE_NAME;
+import static com.thinkaurelius.titan.diskstorage.Backend.ID_STORE_NAME;
+import static com.thinkaurelius.titan.diskstorage.Backend.EDGEINDEX_STORE_NAME;
+import static com.thinkaurelius.titan.diskstorage.Backend.VERTEXINDEX_STORE_NAME;
+
 /**
  * Storage Manager for HBase
  *
@@ -37,6 +42,9 @@ public class HBaseStoreManager extends DistributedStoreManager implements KeyCol
 
     public static final String TABLE_NAME_KEY = "tablename";
     public static final String TABLE_NAME_DEFAULT = "titan";
+    
+    public static final String SHORT_CF_NAMES_KEY = "short-cf-names";
+    public static final boolean SHORT_CF_NAMES_DEFAULT = false;
 
     public static final int PORT_DEFAULT = 9160;
 
@@ -58,6 +66,15 @@ public class HBaseStoreManager extends DistributedStoreManager implements KeyCol
     private final HTablePool connectionPool;
 
     private final StoreFeatures features;
+
+    private final boolean shortCfNames;
+    private static final Map<String, String> shortCfNameMap =
+            ImmutableMap.<String, String>builder()
+                .put(VERTEXINDEX_STORE_NAME, "v")
+                .put(ID_STORE_NAME,          "i")
+                .put(EDGESTORE_NAME,         "s")
+                .put(EDGEINDEX_STORE_NAME,   "e")
+                .build();
 
     public HBaseStoreManager(org.apache.commons.configuration.Configuration config) throws StorageException {
         super(config, PORT_DEFAULT);
@@ -89,7 +106,9 @@ public class HBaseStoreManager extends DistributedStoreManager implements KeyCol
         logger.debug("HBase configuration: set a total of {} configuration values", keysLoaded);
 
         connectionPool = new HTablePool(hconf, connectionPoolSize);
-
+        
+        this.shortCfNames = config.getBoolean(SHORT_CF_NAMES_KEY, SHORT_CF_NAMES_DEFAULT);
+        
         openStores = new ConcurrentHashMap<String, HBaseKeyColumnValueStore>();
 
         // TODO: allowing publicly mutate fields is bad, should be fixed
@@ -163,6 +182,11 @@ public class HBaseStoreManager extends DistributedStoreManager implements KeyCol
         HBaseKeyColumnValueStore store = openStores.get(dbName);
 
         if (store == null) {
+            
+            if (shortCfNames) {
+                dbName = shortenCfName(dbName);
+            }
+            
             HBaseKeyColumnValueStore newStore = new HBaseKeyColumnValueStore(connectionPool, tableName, dbName);
 
             store = openStores.putIfAbsent(dbName, newStore); // nothing bad happens if we loose to other thread
@@ -174,6 +198,19 @@ public class HBaseStoreManager extends DistributedStoreManager implements KeyCol
         }
 
         return store;
+    }
+    
+    private String shortenCfName(String longName) {
+        final String s;
+        if (shortCfNameMap.containsKey(longName)) {
+            s = shortCfNameMap.get(longName);
+            Preconditions.checkNotNull(s);
+            logger.debug("Substituted default CF name \"{}\" with short form \"{}\" to reduce HBase KeyValue size", longName, s);
+        } else {
+            s = longName;
+            logger.debug("Kept default CF name \"{}\" because it has no associated short form", s);
+        }
+        return s;
     }
 
     private HTableDescriptor ensureTableExists(String tableName) throws StorageException {
