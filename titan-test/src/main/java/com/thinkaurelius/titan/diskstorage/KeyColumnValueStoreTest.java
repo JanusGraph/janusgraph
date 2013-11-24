0878f4e562469829fb7ae3de0af4e22c1c775a91
Dan LaRocque <dalaro@hopcount.org>
2013-11-23 23:48:43 -0500

    Reduced loglevel of needlessly severe test log msg
    There was no reason for these two messages to be logged at ERROR.
    They belong at DEBUG or at most INFO.

--- titan-test/src/main/java/com/thinkaurelius/titan/diskstorage/KeyColumnValueStoreTest.java
+++ titan-test/src/main/java/com/thinkaurelius/titan/diskstorage/KeyColumnValueStoreTest.java
@@ -515,7 +515,7 @@ public abstract class KeyColumnValueStoreTest {
                         Collection<StaticBuffer> actual = Sets.newHashSet(i);
 
                         // Check
-                        log.error("Checking bounds [{}, {}) (expect {} keys)",
+                        log.debug("Checking bounds [{}, {}) (expect {} keys)",
                                 new Object[]{startCol, endCol, expected.size()});
                         Assert.assertEquals(expected, actual);
                         i.close();
@@ -551,7 +551,7 @@ public abstract class KeyColumnValueStoreTest {
                         KeyIterator i = store.getKeys(krq, tx);
                         Collection<StaticBuffer> actual = Lists.newArrayList(i);
 
-                        log.error("Checking bounds key:[{}, {}) & col:[{}, {}) (expect {} keys)",
+                        log.debug("Checking bounds key:[{}, {}) & col:[{}, {}) (expect {} keys)",
                                 new Object[]{keyStart, keyEnd, startCol, endCol, expected.size()});
                         Assert.assertEquals(expected, actual);
                         i.close();
