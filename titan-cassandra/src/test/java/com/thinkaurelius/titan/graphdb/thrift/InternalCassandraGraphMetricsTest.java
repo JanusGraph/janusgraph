8acf32098721d8b72223b48a6458a4661a458f8d
Dan LaRocque <dalaro@hopcount.org>
2013-12-23 16:21:06 -0500

    Test import cleanup

--- titan-cassandra/src/test/java/com/thinkaurelius/titan/graphdb/thrift/InternalCassandraGraphMetricsTest.java
+++ titan-cassandra/src/test/java/com/thinkaurelius/titan/graphdb/thrift/InternalCassandraGraphMetricsTest.java
@@ -2,7 +2,6 @@ package com.thinkaurelius.titan.graphdb.thrift;
 
 import com.thinkaurelius.titan.CassandraStorageSetup;
 import com.thinkaurelius.titan.diskstorage.cassandra.CassandraProcessStarter;
-import com.thinkaurelius.titan.graphdb.TitanGraphTest;
 import com.thinkaurelius.titan.graphdb.TitanNonTransactionalGraphMetricsTest;
 import org.apache.commons.configuration.Configuration;
 import org.junit.BeforeClass;
