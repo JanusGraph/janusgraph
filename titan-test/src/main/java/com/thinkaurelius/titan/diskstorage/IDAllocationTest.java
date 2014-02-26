143b20dc4e0aff75a2599bc4ead06c546f81022c
Dan LaRocque <dalaro@hopcount.org>
2014-02-26 15:09:44 -0500

    Increase IDAllocationTest wait time
    10 ms is too quick for CI on slower EC2 instance types

--- titan-test/src/main/java/com/thinkaurelius/titan/diskstorage/IDAllocationTest.java
+++ titan-test/src/main/java/com/thinkaurelius/titan/diskstorage/IDAllocationTest.java
@@ -66,7 +66,7 @@ public abstract class IDAllocationTest {
 
         c = GraphDatabaseConfiguration.buildConfiguration();
         c.set(IDAUTHORITY_UNIQUEID_RETRY_COUNT,10);
-        c.set(IDAUTHORITY_WAIT_MS,10);
+        c.set(IDAUTHORITY_WAIT_MS,100);
         c.set(IDAUTHORITY_UNIQUE_ID_BITS,7);
         c.set(IDAUTHORITY_RANDOMIZE_UNIQUE_ID,true);
         c.set(IDS_BLOCK_SIZE,400);
