067bf4f3c5ed3f534e648e56ea4455b1b08bd9be
Matthias Broecheler <me@matthiasb.com>
2014-05-22 13:46:56 -0600

    Minor reformatting

--- titan-test/src/main/java/com/thinkaurelius/titan/olap/OLAPTest.java
+++ titan-test/src/main/java/com/thinkaurelius/titan/olap/OLAPTest.java
@@ -239,7 +239,7 @@ public abstract class OLAPTest extends TitanGraphBaseTest {
             for (PageRank pr : ranks.values()) {
                 totalDelta+=pr.completeIteration(alpha,numVertices);
             }
-//            System.out.println(String.format("Completed iteration [%s] in time %s ms with delta PR=%s",iteration,w.elapsed(TimeUnit.MILLISECONDS),totalDelta));
+            System.out.println(String.format("Completed iteration [%s] in time %s ms with delta PR=%s",iteration,w.elapsed(TimeUnit.MILLISECONDS),totalDelta));
         } while (totalDelta>PR_TERMINATION_THRESHOLD);
         return ranks;
     }
