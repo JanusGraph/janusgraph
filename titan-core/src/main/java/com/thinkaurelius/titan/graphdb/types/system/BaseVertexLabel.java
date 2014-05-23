6fcb638e6656a1fd160e9f356270cbe0f89bd189
Matthias Broecheler <me@matthiasb.com>
2014-05-23 13:45:14 -0700

    Accidentally did not add file

---
+++ titan-core/src/main/java/com/thinkaurelius/titan/graphdb/types/system/BaseVertexLabel.java
@@ -0,0 +1,39 @@
+package com.thinkaurelius.titan.graphdb.types.system;
+
+import com.thinkaurelius.titan.graphdb.internal.InternalVertexLabel;
+
+/**
+ * @author Matthias Broecheler (me@matthiasb.com)
+ */
+public class BaseVertexLabel extends EmptyVertex implements InternalVertexLabel {
+
+    public static final BaseVertexLabel DEFAULT_VERTEXLABEL = new BaseVertexLabel("_default");
+
+    private final String name;
+
+    public BaseVertexLabel(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean isPartitioned() {
+        return false;
+    }
+
+    @Override
+    public boolean isStatic() {
+        return false;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean hasDefaultConfiguration() {
+        return true;
+    }
+
+
+}
