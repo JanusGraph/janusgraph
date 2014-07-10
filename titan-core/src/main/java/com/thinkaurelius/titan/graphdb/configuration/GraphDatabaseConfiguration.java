049c6ce4f7421403b725498514b99c78a22f589b
Dan LaRocque <dalaro@hopcount.org>
2014-07-10 14:57:04 -0400

    Fix bug in TIMESTAMP_PROVIDER definition
    ConfigOptions store a type token representing the class or interface
    of their values.  ConfigOption has a constructor that tries to guess
    the type token by calling defaultValue.getClass().  Calling getClass()
    on an enum value returns the class of that particular enum value,
    which is likely to be an inner class identifier like EnumName$1.  This
    is almost always not what the writer intended.
    
    We might be able to change ConfigOption's constructor to check whether
    the default value belongs to an enum, and then try to get the enum
    class, but I'm not sure it's worth the complexity.  The user can
    always specify the intended type token by invoking another constructor
    overload.  I've done the latter with TIMESTAMP_PROVIDER.
    
    This fixes a runtime exception whenever we attempted to set and then
    lookup a non-default TIMESTAMP_PROVIDER config setting.

--- titan-core/src/main/java/com/thinkaurelius/titan/graphdb/configuration/GraphDatabaseConfiguration.java
+++ titan-core/src/main/java/com/thinkaurelius/titan/graphdb/configuration/GraphDatabaseConfiguration.java
@@ -154,7 +154,7 @@ public class GraphDatabaseConfiguration {
 
     public static final ConfigOption<Timestamps> TIMESTAMP_PROVIDER = new ConfigOption<Timestamps>(ROOT_NS, "timestamps",
             "The timestamp resolution to use when writing to storage and indices",
-            ConfigOption.Type.FIXED, Timestamps.MICRO);
+            ConfigOption.Type.FIXED, Timestamps.class, Timestamps.MICRO);
 
 
     public static final ConfigOption<Boolean> SYSTEM_LOG_TRANSACTIONS = new ConfigOption<Boolean>(ROOT_NS,"log-tx",
