f44f3318bd22087b3736120a135f092a4c25f202
Dan LaRocque <dalaro@hopcount.org>
2014-07-31 12:13:34 -0400

    Fix bugs in and and extend HadoopConfiguration
    HadoopConfiguration's getKeys(String prefix) method ignored the prefix
    argument.  Besides fixing that bug, this commit extends
    HadoopConfiguration with a new constructor that takes both a
    Configuration object and a String representing an internal key prefix.
    This internal key prefix is hidden from the user but applied whenever
    writing or reading config values.  The idea behind this extension is
    to eventually allow easy construction of a TitanGraph configuration
    atop a subset of Faunus configuration keys sharing a common prefix,
    such as titan.hadoop.input.conf or titan.hadoop.output.conf, without
    necessarily copying the underlying Configuration object.

--- titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/config/HadoopConfiguration.java
+++ titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/config/HadoopConfiguration.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
 
+import com.google.common.base.Predicate;
 import org.apache.hadoop.conf.Configuration;
 import org.elasticsearch.common.collect.Iterators;
 import org.slf4j.Logger;
@@ -18,46 +19,56 @@ import com.thinkaurelius.titan.diskstorage.configuration.WriteConfiguration;
 import com.thinkaurelius.titan.diskstorage.util.time.Durations;
 import com.thinkaurelius.titan.diskstorage.util.time.StandardDuration;
 
+import javax.annotation.Nullable;
+
 public class HadoopConfiguration implements WriteConfiguration {
 
     private static final Logger log =
             LoggerFactory.getLogger(HadoopConfiguration.class);
 
     private final Configuration config;
+    private final String prefix;
 
     public HadoopConfiguration(Configuration config) {
+        this(config, null);
+    }
+
+    public HadoopConfiguration(Configuration config, String prefix) {
         this.config = config;
+        this.prefix = prefix;
     }
 
     @Override
     public <O> O get(String key, Class<O> datatype) {
 
-        if (null == config.get(key))
+        final String internalKey = getInternalKey(key);
+
+        if (null == config.get(internalKey))
             return null;
 
         if (datatype.isArray()) {
             Preconditions.checkArgument(datatype.getComponentType()==String.class,"Only string arrays are supported: %s",datatype);
-            return (O)config.getStrings(key);
+            return (O)config.getStrings(internalKey);
         } else if (Number.class.isAssignableFrom(datatype)) {
-            String s = config.get(key);
+            String s = config.get(internalKey);
             return constructFromStringArgument(datatype, s);
         } else if (datatype==String.class) {
-            return (O)config.get(key);
+            return (O)config.get(internalKey);
         } else if (datatype==Boolean.class) {
-            return (O)Boolean.valueOf(config.get(key));
+            return (O)Boolean.valueOf(config.get(internalKey));
         } else if (datatype.isEnum()) {
             O[] constants = datatype.getEnumConstants();
             Preconditions.checkState(null != constants && 0 < constants.length, "Zero-length or undefined enum");
-            String estr = config.get(key);
+            String estr = config.get(internalKey);
             for (O c : constants)
                 if (c.toString().equals(estr))
                     return c;
             throw new IllegalArgumentException("No match for string \"" + estr + "\" in enum " + datatype);
 //        } else if (datatype==Object.class) {
-//            return (O)config.getProperty(key);
+//            return (O)config.getProperty(internalKey);
         } else if (Duration.class.isAssignableFrom(datatype)) {
             // This is a conceptual leak; the config layer should ideally only handle standard library types
-            String s = config.get(key);
+            String s = config.get(internalKey);
             String[] comps = s.split("\\s");
             TimeUnit unit = null;
             if (comps.length == 1) {
@@ -70,20 +81,46 @@ public class HadoopConfiguration implements WriteConfiguration {
             }
             return (O) new StandardDuration(Long.valueOf(comps[0]), unit);
 //        } else if (List.class.isAssignableFrom(datatype)) {
-//            return (O) config.getProperty(key);
+//            return (O) config.getProperty(internalKey);
         } else throw new IllegalArgumentException("Unsupported data type: " + datatype);
     }
 
     @Override
-    public Iterable<String> getKeys(String prefix) {
+    public Iterable<String> getKeys(final String userPrefix) {
         /*
          * Is there a way to iterate over just the keys of a Hadoop Configuration?
          * Iterating over Map.Entry is needlessly wasteful since we don't need the values.
          */
-        return Iterables.transform(config, new Function<Entry<String, String>, String>() {
+        Iterable<String> internalKeys = Iterables.transform(config, new Function<Entry<String, String>, String>() {
             @Override
-            public String apply(Entry<String, String> input) {
-                return input.getKey();
+            public String apply(Entry<String, String> internalEntry) {
+                return internalEntry.getKey();
+            }
+        });
+
+        Iterable<String> prefixedKeys = Iterables.filter(internalKeys, new Predicate<String>() {
+            @Override
+            public boolean apply(@Nullable String internalKey) {
+                String k = internalKey;
+                if (null != prefix) {
+                    if (k.startsWith(prefix)) {
+                        k = getUserKey(k);
+                    } else {
+                        return false; // does not have the prefix
+                    }
+                }
+                return k.startsWith(userPrefix);
+            }
+        });
+
+        return Iterables.transform(prefixedKeys, new Function<String, String>() {
+
+            @Nullable
+            @Override
+            public String apply(@Nullable String internalKey) {
+                String userKey = getUserKey(internalKey);
+                Preconditions.checkState(userKey.startsWith(userPrefix));
+                return userKey;
             }
         });
     }
@@ -96,38 +133,40 @@ public class HadoopConfiguration implements WriteConfiguration {
     @Override
     public <O> void set(String key, O value) {
 
+        final String internalKey = getInternalKey(key);
+
         Class<?> datatype = value.getClass();
 
         if (datatype.isArray()) {
             Preconditions.checkArgument(datatype.getComponentType()==String.class,"Only string arrays are supported: %s",datatype);
-            config.setStrings(key, (String[])value);
+            config.setStrings(internalKey, (String[])value);
         } else if (Number.class.isAssignableFrom(datatype)) {
-            config.set(key, value.toString());
+            config.set(internalKey, value.toString());
         } else if (datatype==String.class) {
-            config.set(key, value.toString());
+            config.set(internalKey, value.toString());
         } else if (datatype==Boolean.class) {
-            config.setBoolean(key, (Boolean)value);
+            config.setBoolean(internalKey, (Boolean)value);
         } else if (datatype.isEnum()) {
-            config.set(key, value.toString());
+            config.set(internalKey, value.toString());
 //        } else if (datatype==Object.class) {
-//            return (O)config.getProperty(key);
+//            return (O)config.getProperty(internalKey);
         } else if (Duration.class.isAssignableFrom(datatype)) {
             // This is a conceptual leak; the config layer should ideally only handle standard library types
             String millis = String.valueOf(((Duration)value).getLength(TimeUnit.MILLISECONDS));
-            config.set(key, millis);
+            config.set(internalKey, millis);
 //        } else if (List.class.isAssignableFrom(datatype)) {
-//            return (O) config.getProperty(key);
+//            return (O) config.getProperty(internalKey);
         } else throw new IllegalArgumentException("Unsupported data type: " + datatype);
     }
 
     @Override
     public void remove(String key) {
-        config.unset(key);
+        config.unset(getInternalKey(key));
     }
 
     @Override
     public WriteConfiguration copy() {
-        return new HadoopConfiguration(new Configuration(config));
+        return new HadoopConfiguration(new Configuration(config), prefix);
     }
 
     private <O> O constructFromStringArgument(Class<O> datatype, String arg) {
@@ -142,4 +181,19 @@ public class HadoopConfiguration implements WriteConfiguration {
         }
     }
 
+    private String getInternalKey(String userKey) {
+        return null == prefix ? userKey : prefix + userKey;
+    }
+
+    private String getUserKey(String internalKey) {
+        String k = internalKey;
+
+        if (null != prefix) {
+            Preconditions.checkState(k.startsWith(prefix), "key %s does not start with prefix %s", internalKey, prefix);
+            Preconditions.checkState(internalKey.length() > prefix.length());
+            k = internalKey.substring(prefix.length());
+        }
+
+        return k;
+    }
 }
