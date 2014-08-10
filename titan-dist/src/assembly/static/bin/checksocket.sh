8a174f9c20a769b5bacd556770a1ab39c6265b65
Dan LaRocque <dalaro@hopcount.org>
2014-08-10 14:36:52 -0400

    Apply readlink loop from #663 to newer scripts
    The PR didn't cover a few scripts created/introduced on titan05 that
    weren't present on master, the PR's original merge target.
    
    The elasticsearch scripts still do some `dirname $0` calls, but they
    have their own readlink-loop implementation, so I'm leaving them
    alone.

--- titan-dist/src/assembly/static/bin/checksocket.sh
+++ titan-dist/src/assembly/static/bin/checksocket.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-cd "`dirname $0`"/..
+# Returns the absolute path of this script regardless of symlinks
+abs_path() {
+    # From: http://stackoverflow.com/a/246128
+    #   - To resolve finding the directory after symlinks
+    SOURCE="${BASH_SOURCE[0]}"
+    while [ -h "$SOURCE" ]; do
+        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+    done
+    echo "$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+}
+
+cd "abs_path"/..
 
 if [ -n "$JAVA_HOME" ]; then
     JAVA="$JAVA_HOME"/bin/java
