9968d1d9f2bc01f1756db0f36892c2333f101022
Dan LaRocque <dalaro@hopcount.org>
2014-08-10 20:55:46 -0400

    Updated Titan configs used in Expect-based ITs

--- titan-dist/src/test/java/com/thinkaurelius/titan/pkgtest/CassandraThriftESAssemblyIT.java
+++ titan-dist/src/test/java/com/thinkaurelius/titan/pkgtest/CassandraThriftESAssemblyIT.java
@@ -1,5 +1,7 @@
 package com.thinkaurelius.titan.pkgtest;
 
+import com.thinkaurelius.titan.diskstorage.es.ElasticsearchRunner;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -7,13 +9,25 @@ import com.thinkaurelius.titan.CassandraStorageSetup;
 
 public class CassandraThriftESAssemblyIT extends AbstractTitanAssemblyIT {
 
+    public static final String ES_HOME = "../../titan-es";
+
     @BeforeClass
     public static void startCassandra() {
         CassandraStorageSetup.startCleanEmbedded();
     }
 
+    @BeforeClass
+    public static void startES() {
+        new ElasticsearchRunner(ES_HOME).start();
+    }
+
     @Test
     public void testCassandraGettingStarted() throws Exception {
         testGettingStartedGremlinSession("conf/titan-cassandra-es.properties", "cassandrathrift");
+    }
+
+    @AfterClass
+    public static void stopES() {
+        new ElasticsearchRunner(ES_HOME).stop();
     }
 }
--- titan-dist/src/test/java/com/thinkaurelius/titan/pkgtest/FaunusGraphSONIT.java
+++ titan-dist/src/test/java/com/thinkaurelius/titan/pkgtest/FaunusGraphSONIT.java
@@ -6,6 +6,6 @@ public class FaunusGraphSONIT extends AbstractTitanAssemblyIT {
 
     @Test
     public void testGraphOfTheGodsGraphSON() throws Exception {
-        unzipAndRunExpect("faunus-graphson.expect.vm", "conf/hadoop/titan-hadoop.properties", "hadoop");
+        unzipAndRunExpect("faunus-graphson.expect.vm", "conf/hadoop/titan-graphson.properties", "hadoop");
     }
 }
