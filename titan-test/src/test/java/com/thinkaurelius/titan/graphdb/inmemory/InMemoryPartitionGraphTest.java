JanusGraph/janusgraph

commit 1e5e54bf5cfad4aa4c4540887c9ab8f07e933d1f

Require explicit partitioning for part vert labels
Adding a precondition for #706, plus test coverage and some logging
tweaks.  TitanPartitionGraphTest now has tests in both the
OrderedKeyValueStore category (Cassandra BOP, HBase) and the
UnorderedKeyValueStore category (Cassandra Murmur3, RP).  There's a
new test method that checks that addVertexWithLabel fails when the
label is partitioned and explicit cluster partitioning is disabled.
There is also a pair of methods that check whether Titan correctly
guesses whether partitioning should be enabled/disabled based on the
backend StoreFeatures.

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/configuration/GraphDatabaseConfiguration.java
@@ -1220,13 +1220,13 @@ public class GraphDatabaseConfiguration {
             //Freeze global configuration if not already frozen!
             ModifiableConfiguration globalWrite = new ModifiableConfiguration(ROOT_NS,kcvsConfig, BasicConfiguration.Restriction.GLOBAL);
             if (!globalWrite.isFrozen()) {
-                //Copy over global configurations
+                //Copy local config to global
                 Map<ConfigElement.PathIdentifier,Object> allOptions = localbc.getAll();
                 globalWrite.setAll(Maps.filterEntries(allOptions,new Predicate<Map.Entry<ConfigElement.PathIdentifier, Object>>() {
                     @Override
                     public boolean apply(@Nullable Map.Entry<ConfigElement.PathIdentifier, Object> entry) {
                         assert entry.getKey().element.isOption();
-                        return ((ConfigOption)entry.getKey().element).isGlobal();
+                        return ((ConfigOption)entry.getKey().element).isGlobal(); // omits ConfigOption.Type.LOCAL
                     }
                 }));
 
@@ -1238,9 +1238,10 @@ public class GraphDatabaseConfiguration {
                 if (!localbc.has(CLUSTER_PARTITION)) {
                     boolean part = storeFeatures.isDistributed() && storeFeatures.isKeyOrdered();
                     globalWrite.set(CLUSTER_PARTITION, part);
-                    log.info("Enabled partitioning", part);
+                    log.info("Set {}={} from store features", ConfigElement.getPath(CLUSTER_PARTITION), part);
                 } else {
-                    log.info("Disabled partitioning");
+                    log.info("Set {}={} from local config", ConfigElement.getPath(CLUSTER_PARTITION), globalWrite.get(CLUSTER_PARTITION));
+                    Preconditions.checkState(globalWrite.get(CLUSTER_PARTITION).equals(localbc.get(CLUSTER_PARTITION)));
                 }
 
                 /* If the configuration does not explicitly set a timestamp provider and
@@ -1492,6 +1493,10 @@ public class GraphDatabaseConfiguration {
 
     public int getTxDirtyVertexSize() {
         return txDirtyVertexSize;
+    }
+
+    public boolean isClusterPartitioned() {
+        return configuration.get(CLUSTER_PARTITION);
     }
 
     public boolean isBatchLoading() {

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/transaction/StandardTitanTx.java
@@ -13,6 +13,7 @@ import com.thinkaurelius.titan.core.attribute.Cmp;
 import com.thinkaurelius.titan.core.attribute.Duration;
 import com.thinkaurelius.titan.core.schema.*;
 import com.thinkaurelius.titan.diskstorage.BackendException;
+import com.thinkaurelius.titan.diskstorage.configuration.ConfigElement;
 import com.thinkaurelius.titan.diskstorage.util.time.StandardDuration;
 import com.thinkaurelius.titan.diskstorage.util.time.TimestampProvider;
 import com.thinkaurelius.titan.diskstorage.BackendTransaction;
@@ -20,6 +21,7 @@ import com.thinkaurelius.titan.diskstorage.EntryList;
 import com.thinkaurelius.titan.diskstorage.Entry;
 import com.thinkaurelius.titan.diskstorage.keycolumnvalue.SliceQuery;
 import com.thinkaurelius.titan.graphdb.blueprints.TitanBlueprintsTransaction;
+import com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration;
 import com.thinkaurelius.titan.graphdb.database.EdgeSerializer;
 import com.thinkaurelius.titan.graphdb.database.IndexSerializer;
 import com.thinkaurelius.titan.graphdb.database.StandardTitanGraph;
@@ -433,6 +435,8 @@ public class StandardTitanTx extends TitanBlueprintsTransaction implements TypeI
         Preconditions.checkArgument(vertexId == null || IDManager.VertexIDType.NormalVertex.is(vertexId), "Not a valid vertex id: %s", vertexId);
         Preconditions.checkArgument(vertexId == null || ((InternalVertexLabel)label).hasDefaultConfiguration(), "Cannot only use default vertex labels: %s",label);
         Preconditions.checkArgument(vertexId == null || !config.hasVerifyExternalVertexExistence() || !containsVertex(vertexId), "Vertex with given id already exists: %s", vertexId);
+        Preconditions.checkArgument(vertexId != null || !label.isPartitioned() || graph.getConfiguration().isClusterPartitioned(),
+                    "Explicit graph partitioning must be enabled to create a vertex with partitioned label %s", label.getName());
         StandardVertex vertex = new StandardVertex(this, IDManager.getTemporaryVertexID(IDManager.VertexIDType.NormalVertex, temporaryIds.nextID()), ElementLifeCycle.New);
         if (vertexId != null) {
             vertex.setId(vertexId);

titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanPartitionGraphTest.java
@@ -16,6 +16,7 @@ import com.thinkaurelius.titan.graphdb.fulgora.FulgoraBuilder;
 import com.thinkaurelius.titan.graphdb.idmanagement.IDManager;
 import com.thinkaurelius.titan.olap.OLAPTest;
 import com.thinkaurelius.titan.testcategory.OrderedKeyStoreTests;
+import com.thinkaurelius.titan.testcategory.UnorderedKeyStoreTests;
 import com.thinkaurelius.titan.util.datastructures.AbstractLongListUtil;
 import com.tinkerpop.blueprints.Direction;
 import org.junit.Test;
@@ -32,7 +33,6 @@ import static org.junit.Assert.assertEquals;
  *
  * @author Matthias Broecheler (me@matthiasb.com)
  */
-@Category({ OrderedKeyStoreTests.class })
 public abstract class TitanPartitionGraphTest extends TitanGraphBaseTest {
 
     final static Random random = new Random();
@@ -49,7 +49,8 @@ public abstract class TitanPartitionGraphTest extends TitanGraphBaseTest {
     public WriteConfiguration getConfiguration() {
         WriteConfiguration config = getBaseConfiguration();
         ModifiableConfiguration mconf = new ModifiableConfiguration(GraphDatabaseConfiguration.ROOT_NS,config, BasicConfiguration.Restriction.NONE);
-        mconf.set(GraphDatabaseConfiguration.CLUSTER_PARTITION,true);
+        // Let GraphDatabaseConfiguration's config freezer set CLUSTER_PARTITION
+        //mconf.set(GraphDatabaseConfiguration.CLUSTER_PARTITION,true);
         mconf.set(GraphDatabaseConfiguration.CLUSTER_MAX_PARTITIONS,numPartitions);
         //uses SimpleBulkPlacementStrategy by default
         mconf.set(SimpleBulkPlacementStrategy.CONCURRENT_PARTITIONS,16);
@@ -57,6 +58,19 @@ public abstract class TitanPartitionGraphTest extends TitanGraphBaseTest {
     }
 
     @Test
+    @Category({ OrderedKeyStoreTests.class })
+    public void testOrderedConfig() {
+        assertTrue(graph.getConfiguration().isClusterPartitioned());
+    }
+
+    @Test
+    @Category({ UnorderedKeyStoreTests.class })
+    public void testUnorderedConfig() {
+        assertFalse(graph.getConfiguration().isClusterPartitioned());
+    }
+
+    @Test
+    @Category({ OrderedKeyStoreTests.class })
     public void testSetup() {
         final IDManager idManager = graph.getIDManager();
         assertEquals(8, idManager.getPartitionBound());
@@ -67,6 +81,7 @@ public abstract class TitanPartitionGraphTest extends TitanGraphBaseTest {
     }
 
     @Test
+    @Category({ OrderedKeyStoreTests.class })
     public void testVertexPartitioning() throws Exception {
         Object[] options = {option(GraphDatabaseConfiguration.IDS_FLUSH),false};
         clopen(options);
@@ -288,6 +303,19 @@ public abstract class TitanPartitionGraphTest extends TitanGraphBaseTest {
         }
     }
 
+    @Test
+    @Category({ UnorderedKeyStoreTests.class })
+    public void testVLabelOnUnorderedStorage() {
+        final String label = "pl";
+        mgmt.makeVertexLabel(label).partition().make();
+        mgmt.commit();
+        try {
+            graph.addVertexWithLabel(label);
+            fail("Partitioned label must be rejected on unordered key stores");
+        } catch (IllegalArgumentException e) {
+            // Swallow expected exception
+        }
+    }
 
     public static int getPartitionID(TitanVertex vertex, IDManager idManager) {
         long p = idManager.getPartitionId(vertex.getLongId());

commit d5f810b4e2507244871217247a537663986fb76f

Fix InMemoryPartitionGraphTest
I broke this in 1e5e54bf5cfad4aa4c4540887c9ab8f07e933d1f when adding a
mix of partitioned and unpartitioned test methods to its superclass; I
neglected InMemory entirely while thinking about Cassandra and HBase
subclasses which have the same superclass.

titan-test/src/test/java/com/thinkaurelius/titan/graphdb/inmemory/InMemoryPartitionGraphTest.java
@@ -7,6 +7,8 @@ import com.thinkaurelius.titan.graphdb.TitanGraphTest;
 import com.thinkaurelius.titan.graphdb.TitanPartitionGraphTest;
 import com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration;
 
+import static org.junit.Assert.assertFalse;
+
 /**
  * @author Matthias Broecheler (me@matthiasb.com)
  */
@@ -17,6 +19,7 @@ public class InMemoryPartitionGraphTest extends TitanPartitionGraphTest {
     public WriteConfiguration getBaseConfiguration() {
         ModifiableConfiguration config = GraphDatabaseConfiguration.buildConfiguration();
         config.set(GraphDatabaseConfiguration.STORAGE_BACKEND,"inmemory");
+        config.set(GraphDatabaseConfiguration.CLUSTER_PARTITION,true);
         config.set(GraphDatabaseConfiguration.IDS_FLUSH,false);
         return config.getConfiguration();
     }
@@ -26,5 +29,9 @@ public class InMemoryPartitionGraphTest extends TitanPartitionGraphTest {
         newTx();
     }
 
+    @Override
+    public void testUnorderedConfig() { }
 
+    @Override
+    public void testVLabelOnUnorderedStorage() { }
 }
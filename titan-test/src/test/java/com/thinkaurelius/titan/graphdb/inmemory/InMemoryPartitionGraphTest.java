d5f810b4e2507244871217247a537663986fb76f
Dan LaRocque <dalaro@hopcount.org>
2014-09-19 01:56:51 -0400

    Fix InMemoryPartitionGraphTest
    I broke this in 1e5e54bf5cfad4aa4c4540887c9ab8f07e933d1f when adding a
    mix of partitioned and unpartitioned test methods to its superclass; I
    neglected InMemory entirely while thinking about Cassandra and HBase
    subclasses which have the same superclass.

--- titan-test/src/test/java/com/thinkaurelius/titan/graphdb/inmemory/InMemoryPartitionGraphTest.java
+++ titan-test/src/test/java/com/thinkaurelius/titan/graphdb/inmemory/InMemoryPartitionGraphTest.java
@@ -7,6 +7,8 @@ import com.thinkaurelius.titan.graphdb.TitanGraphTest;
 import com.thinkaurelius.titan.graphdb.TitanPartitionGraphTest;
 import com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration;
 
+import static org.junit.Assert.assertFalse;
+
 /**
  * @author Matthias Broecheler (me@matthiasb.com)
  */
@@ -17,6 +19,7 @@ public class InMemoryPartitionGraphTest extends TitanPartitionGraphTest {
     public WriteConfiguration getBaseConfiguration() {
         ModifiableConfiguration config = GraphDatabaseConfiguration.buildConfiguration();
         config.set(GraphDatabaseConfiguration.STORAGE_BACKEND,"inmemory");
+        config.set(GraphDatabaseConfiguration.CLUSTER_PARTITION,true);
         config.set(GraphDatabaseConfiguration.IDS_FLUSH,false);
         return config.getConfiguration();
     }
@@ -26,5 +29,9 @@ public class InMemoryPartitionGraphTest extends TitanPartitionGraphTest {
         newTx();
     }
 
+    @Override
+    public void testUnorderedConfig() { }
 
+    @Override
+    public void testVLabelOnUnorderedStorage() { }
 }
