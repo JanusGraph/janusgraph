9d605dcb292894a5b4c7c9f8512426c1dae9d6d4
Dan LaRocque <dalaro@hopcount.org>
2014-09-19 00:02:21 -0400

    Test tx custom options in graph config
    Check that options set in the config passed to `TitanFactory.open` can
    also be accessed through `BaseTransactionConfig.getCustomOption`

--- titan-cassandra/src/test/java/com/thinkaurelius/titan/graphdb/CassandraGraphTest.java
+++ titan-cassandra/src/test/java/com/thinkaurelius/titan/graphdb/CassandraGraphTest.java
@@ -1,10 +1,18 @@
 package com.thinkaurelius.titan.graphdb;
 
 import com.thinkaurelius.titan.CassandraStorageSetup;
+import com.thinkaurelius.titan.core.TitanFactory;
+import com.thinkaurelius.titan.diskstorage.cassandra.AbstractCassandraStoreManager;
+import com.thinkaurelius.titan.diskstorage.configuration.ConfigElement;
+import com.thinkaurelius.titan.diskstorage.configuration.WriteConfiguration;
+import com.thinkaurelius.titan.graphdb.database.StandardTitanGraph;
+import com.thinkaurelius.titan.graphdb.transaction.StandardTitanTx;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static com.thinkaurelius.titan.diskstorage.cassandra.AbstractCassandraStoreManager.*;
 
 /**
  * @author Joshua Shinavier (http://fortytwo.net)
@@ -24,5 +32,64 @@ public abstract class CassandraGraphTest extends TitanGraphTest {
     @Test
     public void testHasTTL() throws Exception {
         assertTrue(features.hasCellTTL());
+    }
+
+    @Test
+    public void testGraphConfigUsedByThreadBoundTx() {
+        close();
+        WriteConfiguration wc = getConfiguration();
+        wc.set(ConfigElement.getPath(CASSANDRA_READ_CONSISTENCY), "ALL");
+        wc.set(ConfigElement.getPath(CASSANDRA_WRITE_CONSISTENCY), "LOCAL_QUORUM");
+
+        graph = (StandardTitanGraph) TitanFactory.open(wc);
+
+        StandardTitanTx tx = (StandardTitanTx)graph.getCurrentThreadTx();
+        assertEquals("ALL",
+                tx.getTxHandle().getBaseTransactionConfig().getCustomOptions()
+                        .get(AbstractCassandraStoreManager.CASSANDRA_READ_CONSISTENCY));
+        assertEquals("LOCAL_QUORUM",
+                tx.getTxHandle().getBaseTransactionConfig().getCustomOptions()
+                        .get(AbstractCassandraStoreManager.CASSANDRA_WRITE_CONSISTENCY));
+    }
+
+    @Test
+    public void testGraphConfigUsedByTx() {
+        close();
+        WriteConfiguration wc = getConfiguration();
+        wc.set(ConfigElement.getPath(CASSANDRA_READ_CONSISTENCY), "TWO");
+        wc.set(ConfigElement.getPath(CASSANDRA_WRITE_CONSISTENCY), "THREE");
+
+        graph = (StandardTitanGraph) TitanFactory.open(wc);
+
+        StandardTitanTx tx = (StandardTitanTx)graph.newTransaction();
+        assertEquals("TWO",
+                tx.getTxHandle().getBaseTransactionConfig().getCustomOptions()
+                        .get(AbstractCassandraStoreManager.CASSANDRA_READ_CONSISTENCY));
+        assertEquals("THREE",
+                tx.getTxHandle().getBaseTransactionConfig().getCustomOptions()
+                        .get(AbstractCassandraStoreManager.CASSANDRA_WRITE_CONSISTENCY));
+        tx.rollback();
+    }
+
+    @Test
+    public void testCustomConfigUsedByTx() {
+        close();
+        WriteConfiguration wc = getConfiguration();
+        wc.set(ConfigElement.getPath(CASSANDRA_READ_CONSISTENCY), "ALL");
+        wc.set(ConfigElement.getPath(CASSANDRA_WRITE_CONSISTENCY), "ALL");
+
+        graph = (StandardTitanGraph) TitanFactory.open(wc);
+
+        StandardTitanTx tx = (StandardTitanTx)graph.buildTransaction()
+                .setCustomOption(ConfigElement.getPath(CASSANDRA_READ_CONSISTENCY), "ONE")
+                .setCustomOption(ConfigElement.getPath(CASSANDRA_WRITE_CONSISTENCY), "TWO").start();
+
+        assertEquals("ONE",
+                tx.getTxHandle().getBaseTransactionConfig().getCustomOptions()
+                        .get(AbstractCassandraStoreManager.CASSANDRA_READ_CONSISTENCY));
+        assertEquals("TWO",
+                tx.getTxHandle().getBaseTransactionConfig().getCustomOptions()
+                        .get(AbstractCassandraStoreManager.CASSANDRA_WRITE_CONSISTENCY));
+        tx.rollback();
     }
 }
