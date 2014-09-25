0c4e35a44be7f62a440b3c1ea1496356846da0f2
Dan LaRocque <dalaro@hopcount.org>
2014-09-25 13:19:22 -0400

    Enable BP supportsThreadIsolatedTransactions
    Reflectively lookup and set this Blueprints Features field to true
    when it exists, otherwise log and fail silently.  This is done
    reflectively for transparent compatibility with 2.5.0 and 2.6.0
    without having to recompile (it exists in the latter but not in the
    former).

--- titan-core/src/main/java/com/thinkaurelius/titan/graphdb/blueprints/TitanFeatures.java
+++ titan-core/src/main/java/com/thinkaurelius/titan/graphdb/blueprints/TitanFeatures.java
@@ -3,6 +3,10 @@ package com.thinkaurelius.titan.graphdb.blueprints;
 import com.thinkaurelius.titan.diskstorage.keycolumnvalue.StoreFeatures;
 import com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration;
 import com.tinkerpop.blueprints.Features;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
 
 /**
  * Blueprint's features of a TitanGraph.
@@ -11,6 +15,9 @@ import com.tinkerpop.blueprints.Features;
  */
 
 public class TitanFeatures {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(TitanFeatures.class);
 
     private static Features getBaselineTitanFeatures() {
         Features features = new Features();
@@ -43,8 +50,29 @@ public class TitanFeatures {
         features.supportsEdgeRetrieval = true;
         features.supportsTransactions = true;
         features.supportsThreadedTransactions = true;
+        setIfExists(features, "supportsThreadIsolatedTransactions", true);
         features.checkCompliance();
         return features;
+    }
+
+    private static void setIfExists(Features features, String fieldName, boolean value) {
+
+        final Class<?> requiredFieldType = Boolean.class;
+        final Class<?> fc = features.getClass();
+        try {
+            Field f = fc.getField(fieldName);
+            if (!requiredFieldType.equals(f.getType())) {
+                log.debug("Field {}.{} has type {} (but type {} was expected)",
+                        fc, fieldName, f.getType(), requiredFieldType);
+                return;
+            }
+            f.set(features, value);
+            log.debug("Set {}.{} to {}", fc, fieldName, value);
+        } catch (NoSuchFieldException e) {
+            log.debug("Field {}.{} could not be found", fc, fieldName, e);
+        } catch (IllegalAccessException e) {
+            log.warn("Unable to reflectively customize {}.{}", fc, fieldName, e);
+        }
     }
 
     public static Features getFeatures(GraphDatabaseConfiguration config, StoreFeatures storageFeatures) {
