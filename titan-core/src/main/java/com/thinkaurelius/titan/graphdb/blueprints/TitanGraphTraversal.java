bc8d7eaaec89b11e6a0f7b84f3e121681dc41b40
Marko A. Rodriguez <okrammarko@gmail.com>
2014-10-29 15:46:56 -0600

    titan vertex step strategy stub.

--- titan-core/src/main/java/com/thinkaurelius/titan/graphdb/blueprints/TitanGraphTraversal.java
+++ titan-core/src/main/java/com/thinkaurelius/titan/graphdb/blueprints/TitanGraphTraversal.java
@@ -2,8 +2,16 @@ package com.thinkaurelius.titan.graphdb.blueprints;
 
 import com.thinkaurelius.titan.core.TitanGraph;
 import com.thinkaurelius.titan.core.TitanTransaction;
+import com.tinkerpop.gremlin.process.Step;
+import com.tinkerpop.gremlin.process.Traversal;
+import com.tinkerpop.gremlin.process.graph.GraphTraversal;
+import com.tinkerpop.gremlin.process.graph.step.map.VertexStep;
 import com.tinkerpop.gremlin.process.graph.util.DefaultGraphTraversal;
+import com.tinkerpop.gremlin.process.util.TraversalHelper;
+import com.tinkerpop.gremlin.structure.Direction;
+import com.tinkerpop.gremlin.structure.Edge;
 import com.tinkerpop.gremlin.structure.Element;
+import com.tinkerpop.gremlin.structure.Vertex;
 
 /**
  * @author Matthias Broecheler (me@matthiasb.com)
@@ -13,7 +21,21 @@ public class TitanGraphTraversal<S, E> extends DefaultGraphTraversal<S, E> {
     public TitanGraphTraversal(final TitanTransaction graph, final Class<? extends Element> elementClass) {
         super(graph);
         getStrategies().register(TitanGraphStepStrategy.instance());
+        getStrategies().register(TitanVertexStepStrategy.instance());
         addStep(new TitanGraphStep<>(this, elementClass));
     }
+
+    @Override
+    public <E2> GraphTraversal<S, E2> addStep(final Step<?, E2> step) {
+        if (this.getStrategies().complete()) throw Exceptions.traversalIsLocked();
+        TraversalHelper.insertStep((step instanceof VertexStep && ((VertexStep)step).getReturnClass().equals(Edge.class) ? new TitanVertexStep(this,(VertexStep)step) : step, this);
+        return (GraphTraversal) this;
+    }
+
+    @Override
+    public default GraphTraversal<S, Edge> toE(final Direction direction, final int branchFactor, final String... edgeLabels) {
+        return this.addStep(new TitanVertexStep<>(this, Edge.class, direction, branchFactor, edgeLabels));
+    }
+
 
 }
---
+++ titan-core/src/main/java/com/thinkaurelius/titan/graphdb/blueprints/TitanVertexStepStrategy.java
@@ -0,0 +1,36 @@
+package com.thinkaurelius.titan.graphdb.blueprints;
+
+import com.tinkerpop.gremlin.process.Step;
+import com.tinkerpop.gremlin.process.Traversal;
+import com.tinkerpop.gremlin.process.TraversalEngine;
+import com.tinkerpop.gremlin.process.TraversalStrategy;
+import com.tinkerpop.gremlin.process.graph.marker.HasContainerHolder;
+import com.tinkerpop.gremlin.process.util.TraversalHelper;
+
+/**
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
+ */
+public class TitanVertexStepStrategy implements TraversalStrategy.NoDependencies {
+
+    private static final TitanVertexStepStrategy INSTANCE = new TitanVertexStepStrategy();
+
+    private TitanVertexStepStrategy() {
+    }
+
+    @Override
+    public void apply(final Traversal<?, ?> traversal, final TraversalEngine engine) {
+        TraversalHelper.getStepsOfClass(TitanVertexStep.class, traversal).forEach(step -> {
+            Step temp = step.getNextStep();
+            while (temp instanceof HasContainerHolder) {
+                step.addHasContainers(((HasContainerHolder) temp).getHasContainers());
+                TraversalHelper.removeStep(temp, traversal);
+                temp = step.getNextStep();
+            }
+        });
+    }
+
+    public static TitanVertexStepStrategy instance() {
+        return INSTANCE;
+    }
+
+}
