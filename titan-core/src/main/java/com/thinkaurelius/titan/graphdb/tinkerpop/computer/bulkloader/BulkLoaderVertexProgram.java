749150c6b714c1142e2883483c28e9f72d474524
Marko A. Rodriguez <okrammarko@gmail.com>
2014-11-03 18:37:49 -0700

    using ThreadLocal<TitanGraph> to make sure we dont generate too many connections.

--- titan-core/src/main/java/com/thinkaurelius/titan/graphdb/tinkerpop/computer/bulkloader/BulkLoaderVertexProgram.java
+++ titan-core/src/main/java/com/thinkaurelius/titan/graphdb/tinkerpop/computer/bulkloader/BulkLoaderVertexProgram.java
@@ -12,11 +12,17 @@ import com.tinkerpop.gremlin.process.graph.GraphTraversal;
 import com.tinkerpop.gremlin.structure.Edge;
 import com.tinkerpop.gremlin.structure.Graph;
 import com.tinkerpop.gremlin.structure.Vertex;
+import com.tinkerpop.gremlin.structure.util.StringFactory;
+import org.apache.commons.configuration.BaseConfiguration;
 import org.apache.commons.configuration.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 
 /**
@@ -24,7 +30,9 @@ import java.util.Set;
  */
 public class BulkLoaderVertexProgram implements VertexProgram<Long[]> {
 
-    private static final String TITAN_CONFIGURATION_LOCATION = "titan.configuration.location";
+    private static final Logger LOGGER = LoggerFactory.getLogger(BulkLoaderVertexProgram.class);
+
+    private static final String CONFIGURATION_PREFIX = "titan.bulkLoaderVertexProgram.";
     private static final String TITAN_ID = Graph.Key.hide("titan.id");
     private static final Set<String> elementComputeKeys = new HashSet<>();
 
@@ -33,21 +41,27 @@ public class BulkLoaderVertexProgram implements VertexProgram<Long[]> {
     }
 
     private MessageType.Local messageType = MessageType.Local.to(() -> GraphTraversal.<Vertex>of().outE());
-    private TitanGraph graph;
-    private String location;
+    private static ThreadLocal<TitanGraph> graphThreadLocal = new ThreadLocal<>();
+    private Configuration configuration;  // TODO: TitanGraph.configuration() needs to be implemented
 
     private BulkLoaderVertexProgram() {
 
     }
 
     public void storeState(final Configuration configuration) {
-        configuration.setProperty(TITAN_CONFIGURATION_LOCATION, this.location);
+        VertexProgram.super.storeState(configuration);
+        this.configuration.getKeys().forEachRemaining(key -> {
+            configuration.setProperty(CONFIGURATION_PREFIX + key, this.configuration.getProperty(key));
+        });
     }
 
 
     public void loadState(final Configuration configuration) {
-        this.location = configuration.getString(TITAN_CONFIGURATION_LOCATION);
-        this.graph = TitanFactory.open(this.location);
+        this.configuration = new BaseConfiguration();
+        configuration.getKeys().forEachRemaining(key -> {
+            if (key.startsWith(CONFIGURATION_PREFIX))
+                this.configuration.setProperty(key.substring(CONFIGURATION_PREFIX.length()), configuration.getProperty(key));
+        });
     }
 
     @Override
@@ -57,12 +71,19 @@ public class BulkLoaderVertexProgram implements VertexProgram<Long[]> {
 
     @Override
     public void execute(final Vertex vertex, final Messenger<Long[]> messenger, final Memory memory) {
+        if (null == graphThreadLocal.get()) {
+            LOGGER.info("Creating a Titan connection on execute() of " + vertex);
+            graphThreadLocal.set(TitanFactory.open(this.configuration));
+        }
+        final Graph g = graphThreadLocal.get();
+
         if (memory.isInitialIteration()) {
             // create the vertex in titan
-            final Vertex titanVertex = this.graph.addVertex(T.label, vertex.label());
+            final Vertex titanVertex = g.addVertex(T.label, vertex.label());
             // write all the properties of the vertex to the newly created titan vertex
             vertex.properties().forEachRemaining(vertexProperty -> titanVertex.<Object>property(vertexProperty.key(), vertexProperty.value()));
-            this.graph.tx().commit();
+            g.tx().commit();
+            //LOGGER.info("Committing a transaction in vertex writing: " + vertex);
             // vertex.properties().remove();  TODO: optimization to drop data that is not needed in second iteration
             // set a dummy property that is the titan id of this particular vertex
             vertex.property(TITAN_ID, titanVertex.id());
@@ -74,16 +95,17 @@ public class BulkLoaderVertexProgram implements VertexProgram<Long[]> {
             final Map<Long, Long> idPairs = new HashMap<>();
             messenger.receiveMessages(this.messageType).forEach(idPair -> idPairs.put(idPair[0], idPair[1]));
             // get the titan vertex out of titan given the dummy id property
-            final Vertex titanVertex = this.graph.v(vertex.value(TITAN_ID));
+            final Vertex titanVertex = g.v(vertex.value(TITAN_ID));
             // for all the incoming edges of the vertex, get the incoming adjacent vertex and write the edge and its properties
             vertex.inE().forEachRemaining(edge -> {
-                final Vertex incomingAdjacent = this.graph.v(idPairs.get(Long.valueOf(edge.outV().id().next().toString())));
+                final Vertex incomingAdjacent = g.v(idPairs.get(Long.valueOf(edge.outV().id().next().toString())));
                 final Edge titanEdge = incomingAdjacent.addEdge(edge.label(), titanVertex);
                 edge.properties().forEachRemaining(property -> titanEdge.<Object>property(property.key(), property.value()));
             });
             // vertex.bothE().remove(); TODO: optimization to drop data that is not needed any longer
             // vertex.properties().remove(); TODO: optimization to drop data that is not needed any longer
-            this.graph.tx().commit();
+            g.tx().commit();
+            //LOGGER.info("Committing a transaction in edge writing: " + vertex);
         }
     }
 
@@ -95,6 +117,11 @@ public class BulkLoaderVertexProgram implements VertexProgram<Long[]> {
     @Override
     public Set<String> getElementComputeKeys() {
         return elementComputeKeys;
+    }
+
+    @Override
+    public String toString() {
+        return StringFactory.vertexProgramString(this, "");
     }
 
     ////////////////////////
@@ -109,9 +136,15 @@ public class BulkLoaderVertexProgram implements VertexProgram<Long[]> {
             super(BulkLoaderVertexProgram.class);
         }
 
-        public Builder titan(final String location) {
-            this.configuration.setProperty(TITAN_CONFIGURATION_LOCATION, location);
-            return this;
+        public Builder titan(final String propertiesFileLocation) {
+            try {
+                final Properties properties = new Properties();
+                properties.load(new FileInputStream(propertiesFileLocation));
+                properties.forEach((key, value) -> this.configuration.setProperty(CONFIGURATION_PREFIX + key, value));
+                return this;
+            } catch (final Exception e) {
+                throw new IllegalArgumentException(e.getMessage(), e);
+            }
         }
 
     }
