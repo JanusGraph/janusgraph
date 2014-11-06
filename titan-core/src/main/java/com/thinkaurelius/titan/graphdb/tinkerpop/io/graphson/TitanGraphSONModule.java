822a1061f8b985d12cd2b1b9616fcad899b5f1a9
Stephen Mallette <spmva@genoprime.com>
2014-11-06 08:35:19 -0500

    GraphSON tests now pass.
    Added a custom GraphSON serializer for RelationIdentifier and provided methods that help the test-suite deal with that class.

---
+++ titan-core/src/main/java/com/thinkaurelius/titan/graphdb/tinkerpop/io/graphson/TitanGraphSONModule.java
@@ -0,0 +1,45 @@
+package com.thinkaurelius.titan.graphdb.tinkerpop.io.graphson;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.thinkaurelius.titan.graphdb.relations.RelationIdentifier;
+
+import java.io.IOException;
+
+/**
+ * @author Stephen Mallette (http://stephen.genoprime.com)
+ */
+public class TitanGraphSONModule extends SimpleModule {
+    public TitanGraphSONModule() {
+        addSerializer(RelationIdentifier.class, new RelationIdentifierSerializer());
+    }
+
+    public static class RelationIdentifierSerializer extends StdSerializer<RelationIdentifier> {
+
+        public RelationIdentifierSerializer() {
+            super(RelationIdentifier.class);
+        }
+
+        @Override
+        public void serialize(final RelationIdentifier relationIdentifier, final JsonGenerator jsonGenerator,
+                              final SerializerProvider serializerProvider) throws IOException, JsonGenerationException {
+            ser(relationIdentifier, jsonGenerator);
+        }
+
+        @Override
+        public void serializeWithType(final RelationIdentifier relationIdentifier, final JsonGenerator jsonGenerator,
+                                      final SerializerProvider serializerProvider, final TypeSerializer typeSerializer) throws IOException, JsonProcessingException {
+            ser(relationIdentifier, jsonGenerator);
+        }
+
+        private void ser(final RelationIdentifier relationIdentifier, final JsonGenerator jsonGenerator)
+                throws IOException {
+            jsonGenerator.writeString(relationIdentifier.toString());
+        }
+    }
+}
