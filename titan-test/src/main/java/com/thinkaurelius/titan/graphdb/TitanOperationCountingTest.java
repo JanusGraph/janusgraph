4499f2ef6c2a96320b7b263d2b2237ad87f80081
Matthias Broecheler <me@matthiasb.com>
2014-11-14 18:31:53 -0800

    Removed assertion that is time dependent.

--- titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanOperationCountingTest.java
+++ titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanOperationCountingTest.java
@@ -616,7 +616,7 @@ public abstract class TitanOperationCountingTest extends TitanGraphBaseTest {
 
         System.out.println(round(timecoldglobal) + "\t" + round(timewarmglobal) + "\t" + round(timehotglobal));
         assertTrue(timecoldglobal + " vs " + timewarmglobal, timecoldglobal>timewarmglobal*2);
-        assertTrue(timewarmglobal + " vs " + timehotglobal, timewarmglobal>timehotglobal);
+        //assertTrue(timewarmglobal + " vs " + timehotglobal, timewarmglobal>timehotglobal); Sometimes, this is not true
     }
 
     private double testAllVertices(long vid, int numV) {
