37b3a45a2fe440c1aa2c845ae1f0fb49c5ce0ef8
Dan LaRocque <dalaro@hopcount.org>
2014-11-25 20:59:27 -0500

    Tracking TP3 changes for InputFormats
    This commit follows Marko's recent repackaging of giraph-gremlin into
    hadoop-gremlin.  This commit deletes GiraphInputFormat and
    GiraphVertexInputFormat.  The logic in these classes is now in
    hadoop-gremlin.  Titan just needs to provide an
    InputFormat<NullWritable, VertexWritable> that reads vertices from
    Titan and produces VertexWritable (a thin wrapper around TinkerVertex
    right now).  This has simplified things on the Titan side.
    
    I tested the Cassandra IF and BulkLoader against TP 3.0.0-SNAPSHOT at
    commit 7a79fc1b531d2871dffcaf241de712bbfb116efe.

--- titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/formats/util/GiraphInputFormat.java
+++ titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/formats/util/GiraphInputFormat.java
@@ -5,7 +5,7 @@ import com.thinkaurelius.titan.diskstorage.Entry;
 import com.thinkaurelius.titan.diskstorage.StaticBuffer;
 import com.thinkaurelius.titan.hadoop.formats.util.input.TitanHadoopSetup;
 import com.thinkaurelius.titan.util.system.ConfigurationUtil;
-import com.tinkerpop.gremlin.giraph.process.computer.GiraphComputeVertex;
+import com.tinkerpop.gremlin.hadoop.structure.io.VertexWritable;
 import org.apache.hadoop.conf.Configurable;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.NullWritable;
@@ -17,7 +17,7 @@ import java.util.List;
 import static com.thinkaurelius.titan.hadoop.formats.util.input.TitanHadoopSetupCommon.SETUP_CLASS_NAME;
 import static com.thinkaurelius.titan.hadoop.formats.util.input.TitanHadoopSetupCommon.SETUP_PACKAGE_PREFIX;
 
-public abstract class GiraphInputFormat extends InputFormat<NullWritable, GiraphComputeVertex> implements Configurable {
+public abstract class GiraphInputFormat extends InputFormat<NullWritable, VertexWritable> implements Configurable {
 
     private final InputFormat<StaticBuffer, Iterable<Entry>> inputFormat;
     private TitanVertexDeserializer vertexDeserializer;
@@ -33,7 +33,7 @@ public abstract class GiraphInputFormat extends InputFormat<NullWritable, Giraph
     }
 
     @Override
-    public RecordReader<NullWritable, GiraphComputeVertex> createRecordReader(InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
+    public RecordReader<NullWritable, VertexWritable> createRecordReader(InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
         return new GiraphRecordReader(getVertexDeserializer(), inputFormat.createRecordReader(split, context));
     }
 
