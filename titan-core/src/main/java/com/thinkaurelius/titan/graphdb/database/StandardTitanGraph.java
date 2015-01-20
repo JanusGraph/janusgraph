thinkaurelius/titan

Use consistent tx for schema retrieval
Experimenting with opening a new transaction with a reference held on
the stack solely for the purpose of reading schema vertices and schema
relations.

For #881

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/database/StandardTitanGraph.java
@@ -9,6 +9,9 @@ import com.thinkaurelius.titan.core.*;
 import com.thinkaurelius.titan.core.Cardinality;
 import com.thinkaurelius.titan.core.schema.*;
 import com.thinkaurelius.titan.core.Multiplicity;
+import com.thinkaurelius.titan.diskstorage.configuration.Configuration;
+import com.thinkaurelius.titan.diskstorage.configuration.MergedConfiguration;
+import com.thinkaurelius.titan.diskstorage.util.StandardBaseTransactionConfig;
 import com.thinkaurelius.titan.diskstorage.util.StaticArrayEntry;
 import com.thinkaurelius.titan.diskstorage.log.Message;
 import com.thinkaurelius.titan.diskstorage.log.ReadMarker;
@@ -288,19 +291,36 @@ public class StandardTitanGraph extends TitanBlueprintsGraph {
 
         @Override
         public Long retrieveSchemaByName(String typeName, StandardTitanTx tx) {
-            tx.getTxHandle().disableCache(); //Disable cache to make sure that schema is only cached once and cache eviction works!
-            TitanVertex v = Iterables.getOnlyElement(tx.getVertices(BaseKey.SchemaName, typeName),null);
-            tx.getTxHandle().enableCache();
-            return v!=null?v.getLongId():null;
+            // Get a consistent tx
+            Configuration customTxOptions = new MergedConfiguration(backend.getStoreFeatures().getKeyConsistentTxConfig(),
+                    tx.getConfiguration().getCustomOptions());
+            TitanTransaction consistentTx = null;
+            try {
+                consistentTx = StandardTitanGraph.this.newTransaction(new StandardTransactionBuilder(getConfiguration(), StandardTitanGraph.this, customTxOptions));
+                tx.getTxHandle().disableCache(); //Disable cache to make sure that schema is only cached once and cache eviction works!
+                TitanVertex v = Iterables.getOnlyElement(consistentTx.getVertices(BaseKey.SchemaName, typeName), null);
+                tx.getTxHandle().enableCache();
+                return v!=null?v.getLongId():null;
+            } finally {
+                consistentTx.rollback();
+            }
         }
 
         @Override
         public EntryList retrieveSchemaRelations(final long schemaId, final BaseRelationType type, final Direction dir, final StandardTitanTx tx) {
             SliceQuery query = queryCache.getQuery(type,dir);
-            tx.getTxHandle().disableCache(); //Disable cache to make sure that schema is only cached once!
-            EntryList result = edgeQuery(schemaId, query, tx.getTxHandle());
-            tx.getTxHandle().enableCache();
-            return result;
+            Configuration customTxOptions = new MergedConfiguration(backend.getStoreFeatures().getKeyConsistentTxConfig(),
+                    tx.getConfiguration().getCustomOptions());
+            StandardTitanTx consistentTx = null;
+            try {
+                consistentTx = StandardTitanGraph.this.newTransaction(new StandardTransactionBuilder(getConfiguration(), StandardTitanGraph.this, customTxOptions));
+                tx.getTxHandle().disableCache(); //Disable cache to make sure that schema is only cached once!
+                EntryList result = edgeQuery(schemaId, query, consistentTx.getTxHandle());
+                tx.getTxHandle().enableCache();
+                return result;
+            } finally {
+                consistentTx.rollback();
+            }
         }
 
     };

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/transaction/StandardTransactionBuilder.java
@@ -89,6 +89,24 @@ public class StandardTransactionBuilder implements TransactionConfiguration, Tra
         setDirtyVertexSize(graphConfig.getTxDirtyVertexSize());
     }
 
+    public StandardTransactionBuilder(GraphDatabaseConfiguration graphConfig, StandardTitanGraph graph, Configuration customOptions) {
+        Preconditions.checkNotNull(graphConfig);
+        Preconditions.checkNotNull(graph);
+        if (graphConfig.isReadOnly()) readOnly();
+        if (graphConfig.isBatchLoading()) enableBatchLoading();
+        this.graph = graph;
+        this.defaultSchemaMaker = graphConfig.getDefaultSchemaMaker();
+        this.assignIDsImmediately = graphConfig.hasFlushIDs();
+        this.forceIndexUsage = graphConfig.hasForceIndexUsage();
+        this.groupName = graphConfig.getMetricsPrefix();
+        this.logIdentifier = null;
+        this.propertyPrefetching = graphConfig.hasPropertyPrefetching();
+        this.writableCustomOptions = null;
+        this.customOptions = customOptions;
+        setVertexCacheSize(graphConfig.getTxVertexCacheSize());
+        setDirtyVertexSize(graphConfig.getTxDirtyVertexSize());
+    }
+
     public StandardTransactionBuilder threadBound() {
         this.threadBound = true;
         this.singleThreaded = true;
@@ -183,6 +201,9 @@ public class StandardTransactionBuilder implements TransactionConfiguration, Tra
 
     @Override
     public TransactionBuilder setCustomOption(String k, Object v) {
+        if (null == writableCustomOptions)
+            throw new IllegalStateException("This builder was not constructed with setCustomOption support");
+
         writableCustomOptions.set((ConfigOption<Object>)ConfigElement.parse(ROOT_NS, k).element, v);
         return this;
     }

JanusGraph/janusgraph

(cherry picked from commit bc88a4598c16486efd191c4a327ccd077c0beac2)
Conflicts:
	titan-core/src/main/java/com/thinkaurelius/titan/graphdb/database/StandardTitanGraph.java
	titan-core/src/main/java/com/thinkaurelius/titan/graphdb/transaction/StandardTransactionBuilder.java

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/database/StandardTitanGraph.java
@@ -9,6 +9,9 @@ import com.thinkaurelius.titan.core.*;
 import com.thinkaurelius.titan.core.Cardinality;
 import com.thinkaurelius.titan.core.schema.*;
 import com.thinkaurelius.titan.core.Multiplicity;
+import com.thinkaurelius.titan.diskstorage.configuration.Configuration;
+import com.thinkaurelius.titan.diskstorage.configuration.MergedConfiguration;
+import com.thinkaurelius.titan.diskstorage.util.StandardBaseTransactionConfig;
 import com.thinkaurelius.titan.diskstorage.util.StaticArrayEntry;
 import com.thinkaurelius.titan.diskstorage.log.Message;
 import com.thinkaurelius.titan.diskstorage.log.ReadMarker;
@@ -362,19 +365,36 @@ private synchronized void removeHook() {
 
         @Override
         public Long retrieveSchemaByName(String typeName, StandardTitanTx tx) {
-            tx.getTxHandle().disableCache(); //Disable cache to make sure that schema is only cached once and cache eviction works!
-            TitanVertex v = Iterables.getOnlyElement(QueryUtil.getVertices(tx,BaseKey.SchemaName, typeName),null);
-            tx.getTxHandle().enableCache();
-            return v!=null?v.longId():null;
+            // Get a consistent tx
+            Configuration customTxOptions = new MergedConfiguration(backend.getStoreFeatures().getKeyConsistentTxConfig(),
+                    tx.getConfiguration().getCustomOptions());
+            TitanTransaction consistentTx = null;
+            try {
+                consistentTx = StandardTitanGraph.this.newTransaction(new StandardTransactionBuilder(getConfiguration(), StandardTitanGraph.this, customTxOptions));
+                tx.getTxHandle().disableCache(); //Disable cache to make sure that schema is only cached once and cache eviction works!
+                TitanVertex v = Iterables.getOnlyElement(QueryUtil.getVertices(tx, BaseKey.SchemaName, typeName), null);
+                tx.getTxHandle().enableCache();
+                return v!=null?v.longId():null;
+            } finally {
+                consistentTx.rollback();
+            }
         }
 
         @Override
         public EntryList retrieveSchemaRelations(final long schemaId, final BaseRelationType type, final Direction dir, final StandardTitanTx tx) {
             SliceQuery query = queryCache.getQuery(type,dir);
-            tx.getTxHandle().disableCache(); //Disable cache to make sure that schema is only cached once!
-            EntryList result = edgeQuery(schemaId, query, tx.getTxHandle());
-            tx.getTxHandle().enableCache();
-            return result;
+            Configuration customTxOptions = new MergedConfiguration(backend.getStoreFeatures().getKeyConsistentTxConfig(),
+                    tx.getConfiguration().getCustomOptions());
+            StandardTitanTx consistentTx = null;
+            try {
+                consistentTx = StandardTitanGraph.this.newTransaction(new StandardTransactionBuilder(getConfiguration(), StandardTitanGraph.this, customTxOptions));
+                tx.getTxHandle().disableCache(); //Disable cache to make sure that schema is only cached once!
+                EntryList result = edgeQuery(schemaId, query, consistentTx.getTxHandle());
+                tx.getTxHandle().enableCache();
+                return result;
+            } finally {
+                consistentTx.rollback();
+            }
         }
 
     };

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/transaction/StandardTransactionBuilder.java
@@ -91,6 +91,24 @@ public class StandardTransactionBuilder implements TransactionConfiguration, Tra
         dirtyVertexSize(graphConfig.getTxDirtyVertexSize());
     }
 
+    public StandardTransactionBuilder(GraphDatabaseConfiguration graphConfig, StandardTitanGraph graph, Configuration customOptions) {
+        Preconditions.checkNotNull(graphConfig);
+        Preconditions.checkNotNull(graph);
+        if (graphConfig.isReadOnly()) readOnly();
+        if (graphConfig.isBatchLoading()) enableBatchLoading();
+        this.graph = graph;
+        this.defaultSchemaMaker = graphConfig.getDefaultSchemaMaker();
+        this.assignIDsImmediately = graphConfig.hasFlushIDs();
+        this.forceIndexUsage = graphConfig.hasForceIndexUsage();
+        this.groupName = graphConfig.getMetricsPrefix();
+        this.logIdentifier = null;
+        this.propertyPrefetching = graphConfig.hasPropertyPrefetching();
+        this.writableCustomOptions = null;
+        this.customOptions = customOptions;
+        vertexCacheSize(graphConfig.getTxVertexCacheSize());
+        dirtyVertexSize(graphConfig.getTxDirtyVertexSize());
+    }
+
     public StandardTransactionBuilder threadBound() {
         this.threadBound = true;
         this.singleThreaded = true;
@@ -190,6 +208,8 @@ public class StandardTransactionBuilder implements TransactionConfiguration, Tra
 
     @Override
     public TransactionBuilder customOption(String k, Object v) {
+        if (null == writableCustomOptions)
+            throw new IllegalStateException("This builder was not constructed with setCustomOption support");
         writableCustomOptions.set((ConfigOption<Object>)ConfigElement.parse(ROOT_NS, k).element, v);
         return this;
     }