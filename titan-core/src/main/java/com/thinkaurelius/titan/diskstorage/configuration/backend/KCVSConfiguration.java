thinkaurelius/titan

Support ThreadLocal and CHM for Kryo caching
This commit adds the `graph.kryo-instance-cache` option.  It takes
either the value THREAD_LOCAL (default) or CONCURRENT_HASH_MAP.  This
commit effectively restores the old Kryo caching behavior of
KryoSerializer, where it stored Kryos in a ThreadLocal, and where
those ThreadLocals are not necessarily removed when Titan shuts down.
The CHM setting provides an alternative that, unlike ThreadLocal, is
exposed to synchronization overhead when retrieving cached Kryos, but
which also releases all of its cached Kryo references when the graph
is shutdown.

For #840

titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/configuration/backend/KCVSConfiguration.java
@@ -21,9 +21,11 @@ import com.thinkaurelius.titan.diskstorage.util.BackendOperation;
 import com.thinkaurelius.titan.diskstorage.util.BufferUtil;
 import com.thinkaurelius.titan.diskstorage.util.StaticArrayBuffer;
 import com.thinkaurelius.titan.diskstorage.util.StaticArrayEntry;
+import com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration;
 import com.thinkaurelius.titan.graphdb.database.serialize.DataOutput;
 import com.thinkaurelius.titan.graphdb.database.serialize.StandardSerializer;
 
+import com.thinkaurelius.titan.graphdb.database.serialize.kryo.KryoInstanceCacheImpl;
 import com.thinkaurelius.titan.util.system.IOUtils;
 import org.apache.commons.lang.StringUtils;
 
@@ -50,9 +52,13 @@ public class KCVSConfiguration implements ConcurrentWriteConfiguration {
 
     private Duration maxOperationWaitTime = new StandardDuration(10000L, TimeUnit.MILLISECONDS);
 
-
     public KCVSConfiguration(BackendOperation.TransactionalProvider txProvider, TimestampProvider times,
                              KeyColumnValueStore store, String identifier) throws BackendException {
+        this(txProvider, times, store, identifier, GraphDatabaseConfiguration.KRYO_INSTANCE_CACHE.getDefaultValue());
+    }
+
+    public KCVSConfiguration(BackendOperation.TransactionalProvider txProvider, TimestampProvider times,
+                             KeyColumnValueStore store, String identifier, KryoInstanceCacheImpl kcache) throws BackendException {
         Preconditions.checkArgument(txProvider!=null && store!=null && times!=null);
         Preconditions.checkArgument(StringUtils.isNotBlank(identifier));
         this.txProvider = txProvider;
@@ -60,7 +66,7 @@ public class KCVSConfiguration implements ConcurrentWriteConfiguration {
         this.store = store;
         this.identifier = identifier;
         this.rowKey = string2StaticBuffer(this.identifier);
-        this.serializer = new StandardSerializer();
+        this.serializer = new StandardSerializer(true, kcache);
     }
 
     public void setMaxOperationWaitTime(Duration waitTime) {

JanusGraph/janusgraph

(cherry picked from commit 1f6f9503c5a6120da4cba0dea8fbab6fb27624be)
Conflicts:
	titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/Backend.java
	titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/configuration/backend/KCVSConfiguration.java
	titan-core/src/main/java/com/thinkaurelius/titan/graphdb/database/serialize/StandardSerializer.java
	titan-core/src/main/java/com/thinkaurelius/titan/graphdb/database/serialize/kryo/KryoSerializer.java

titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/configuration/backend/KCVSConfiguration.java
@@ -22,9 +22,11 @@ import com.thinkaurelius.titan.diskstorage.util.BackendOperation;
 import com.thinkaurelius.titan.diskstorage.util.BufferUtil;
 import com.thinkaurelius.titan.diskstorage.util.StaticArrayBuffer;
 import com.thinkaurelius.titan.diskstorage.util.StaticArrayEntry;
+import com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration;
 import com.thinkaurelius.titan.graphdb.database.serialize.DataOutput;
 import com.thinkaurelius.titan.graphdb.database.serialize.StandardSerializer;
 
+import com.thinkaurelius.titan.graphdb.database.serialize.kryo.KryoInstanceCacheImpl;
 import com.thinkaurelius.titan.util.system.IOUtils;
 import com.tinkerpop.gremlin.structure.Graph;
 import org.apache.commons.lang.StringUtils;
@@ -55,9 +57,13 @@ public class KCVSConfiguration implements ConcurrentWriteConfiguration {
 
     private Duration maxOperationWaitTime = new StandardDuration(10000L, TimeUnit.MILLISECONDS);
 
-
     public KCVSConfiguration(BackendOperation.TransactionalProvider txProvider, Configuration config,
                              KeyColumnValueStore store, String identifier) throws BackendException {
+        this(txProvider, config, store, identifier, GraphDatabaseConfiguration.KRYO_INSTANCE_CACHE.getDefaultValue());
+    }
+
+    public KCVSConfiguration(BackendOperation.TransactionalProvider txProvider, Configuration config,
+                             KeyColumnValueStore store, String identifier, KryoInstanceCacheImpl kcache) throws BackendException {
         Preconditions.checkArgument(txProvider!=null && store!=null && config!=null);
         Preconditions.checkArgument(StringUtils.isNotBlank(identifier));
         this.txProvider = txProvider;
@@ -65,7 +71,7 @@ public class KCVSConfiguration implements ConcurrentWriteConfiguration {
         this.store = store;
         this.identifier = identifier;
         this.rowKey = string2StaticBuffer(this.identifier);
-        this.serializer = new StandardSerializer(); //config.get(ATTRIBUTE_ALLOW_ALL_SERIALIZABLE)
+        this.serializer = new StandardSerializer(true, kcache); //config.get(ATTRIBUTE_ALLOW_ALL_SERIALIZABLE)
     }
 
     public void setMaxOperationWaitTime(Duration waitTime) {