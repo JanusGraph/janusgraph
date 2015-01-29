cae94d54517da0e3350bf8cd08ef3016fb0f0874
Dan LaRocque <dalaro@hopcount.org>
2015-01-29 02:18:10 -0500

    Committing a (weak) bulk load schema checker
    This modifies BulkLoaderVertexProgram with optional and very limited
    data integrity checks.  The checks only look at edge multiplicity and
    property cardinality constraints verifiable from the perspective of a
    single vertex.  They're memory inefficient.  They're incapable of
    detecting violations involving relations already in Titan (incremental
    loading).
    
    This implementation started out as a TP3 MapReduce job before I
    noticed that MR jobs couldn't be placed before VertexPrograms in the
    computer model, then as a VertexProgram before I noticed that only one
    VP is allowed per computer execution, and then finally as a bunch of
    conditional-enclosed paths inside BulkLoaderVertexProgram.
    
    I'm not even convinced the weak guarantees theoretically offered by
    this change is worth the extra complexity.  It's off by default.

--- titan-core/src/main/java/com/thinkaurelius/titan/graphdb/tinkerpop/computer/bulkloader/BulkLoaderVertexProgram.java
+++ titan-core/src/main/java/com/thinkaurelius/titan/graphdb/tinkerpop/computer/bulkloader/BulkLoaderVertexProgram.java
@@ -1,8 +1,11 @@
 package com.thinkaurelius.titan.graphdb.tinkerpop.computer.bulkloader;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
-import com.thinkaurelius.titan.core.TitanFactory;
-import com.thinkaurelius.titan.core.TitanGraph;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Iterators;
+import com.thinkaurelius.titan.core.*;
+import com.thinkaurelius.titan.core.schema.TitanManagement;
 import com.thinkaurelius.titan.util.system.IOUtils;
 import com.tinkerpop.gremlin.process.T;
 import com.tinkerpop.gremlin.process.computer.Memory;
@@ -13,7 +16,6 @@ import com.tinkerpop.gremlin.process.computer.util.AbstractVertexProgramBuilder;
 import com.tinkerpop.gremlin.process.graph.AnonymousGraphTraversal;
 import com.tinkerpop.gremlin.process.graph.GraphTraversal;
 import com.tinkerpop.gremlin.structure.Edge;
-import com.tinkerpop.gremlin.structure.Graph;
 import com.tinkerpop.gremlin.structure.Vertex;
 import com.tinkerpop.gremlin.structure.VertexProperty;
 import com.tinkerpop.gremlin.structure.util.StringFactory;
@@ -23,10 +25,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
@@ -37,13 +36,19 @@ public class BulkLoaderVertexProgram implements VertexProgram<Long[]> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BulkLoaderVertexProgram.class);
 
-    private static final String CONFIGURATION_PREFIX = "titan.bulkLoaderVertexProgram.";
+    // TODO this is a step backwards in config management
+    private static final String CFG_GRAPH_PREFIX = "titan.bulkload.graphconfig.";
+    private static final String CFG_SCHEMA_CHECK = "titan.bulkload.schemacheck";
+    private static final boolean CFG_SCHEMA_CHECK_DEFAULT = false;
+
     private static final String TITAN_ID = "titan.id";
     private static final ImmutableSet<String> elementComputeKeys = ImmutableSet.of(TITAN_ID);
 
     private MessageScope messageScope = MessageScope.Local.of(AnonymousGraphTraversal.Tokens.__::inE);
     private Configuration configuration;
     private TitanGraph graph;
+    private TitanManagement mgmt;
+    private boolean checkSchema;
 
     private BulkLoaderVertexProgram() {
 
@@ -52,17 +57,19 @@ public class BulkLoaderVertexProgram implements VertexProgram<Long[]> {
     public void storeState(final Configuration configuration) {
         VertexProgram.super.storeState(configuration);
         this.configuration.getKeys().forEachRemaining(key -> {
-            configuration.setProperty(CONFIGURATION_PREFIX + key, this.configuration.getProperty(key));
+            configuration.setProperty(CFG_GRAPH_PREFIX + key, this.configuration.getProperty(key));
         });
+        configuration.setProperty(CFG_SCHEMA_CHECK, checkSchema);
     }
 
 
     public void loadState(final Configuration configuration) {
         this.configuration = new BaseConfiguration();
         configuration.getKeys().forEachRemaining(key -> {
-            if (key.startsWith(CONFIGURATION_PREFIX))
-                this.configuration.setProperty(key.substring(CONFIGURATION_PREFIX.length()), configuration.getProperty(key));
+            if (key.startsWith(CFG_GRAPH_PREFIX))
+                this.configuration.setProperty(key.substring(CFG_GRAPH_PREFIX.length()), configuration.getProperty(key));
         });
+        checkSchema = configuration.getBoolean(CFG_SCHEMA_CHECK, CFG_SCHEMA_CHECK_DEFAULT);
     }
 
     @Override
@@ -73,15 +80,25 @@ public class BulkLoaderVertexProgram implements VertexProgram<Long[]> {
     @Override
     public void workerIterationStart(final Memory memory) {
         if (null == graph) {
+            Preconditions.checkState(null == mgmt);
             graph = TitanFactory.open(configuration);
             LOGGER.info("Opened TitanGraph instance: {}", graph);
+            mgmt = graph.openManagement();
+            LOGGER.info("Opened TitanManagement instance: {}", mgmt);
         } else {
-            LOGGER.warn("Leaked TitanGraph instance: {}", graph);
+            LOGGER.warn("Leaked TitanGraph/TitanManagement instance(s): {}/{}", graph, mgmt);
         }
     }
 
     @Override
     public void workerIterationEnd(final Memory memory) {
+        if (null != mgmt) {
+            LOGGER.info("Rolling back management system: {}", mgmt);
+            mgmt.rollback();
+            LOGGER.info("Rolled back management system: {}", mgmt);
+            mgmt = null;
+        }
+
         if (null != graph) {
             LOGGER.info("Committing transaction on TitanGraph instance: {}", graph);
             graph.tx().commit(); // TODO will Giraph/MR restart the program and re-run execute if this fails?
@@ -97,6 +114,11 @@ public class BulkLoaderVertexProgram implements VertexProgram<Long[]> {
         if (memory.isInitialIteration()) {
             // create the vertex in titan
             final Vertex titanVertex = graph.addVertex(T.label, vertex.label());
+            // enforce property constraints if so configured
+            if (checkSchema) {
+                LOGGER.debug("Checking properties on vertex {}", vertex);
+                checkProperties(vertex);
+            }
             // write all the properties of the vertex to the newly created titan vertex
             vertex.properties().forEachRemaining(vertexProperty -> {
                 // Set properties
@@ -116,17 +138,172 @@ public class BulkLoaderVertexProgram implements VertexProgram<Long[]> {
             final Map<Long, Long> idPairs = new HashMap<>();
             messenger.receiveMessages(this.messageScope).forEach(idPair -> idPairs.put(idPair[0], idPair[1]));
             // get the titan vertex out of titan given the dummy id property
-            final Vertex titanVertex = graph.V(vertex.value(TITAN_ID)).next();
+            Object vid = vertex.value(TITAN_ID);
+            final Vertex titanVertex = graph.V(vid).next();
+            // enforce edge constraints if so configured
+            if (checkSchema) {
+                LOGGER.debug("Checking edges on vertex {}", vertex);
+                checkEdges(vertex);
+            }
             // for all the incoming edges of the vertex, get the incoming adjacent vertex and write the edge and its properties
             vertex.outE().forEachRemaining(edge -> {
                 final Vertex outgoingAdjacent = graph.V(idPairs.get(Long.valueOf(edge.inV().id().next().toString()))).next();
                 final Edge titanEdge = titanVertex.addEdge(edge.label(), outgoingAdjacent);
                 edge.properties().forEachRemaining(property -> titanEdge.<Object>property(property.key(), property.value()));
             });
+
             // vertex.bothE().remove(); TODO: optimization to drop data that is not needed any longer
             // vertex.properties().remove(); TODO: optimization to drop data that is not needed any longer
             //LOGGER.info("Committing a transaction in edge writing: " + vertex);
         }
+    }
+
+    private void checkProperties(Vertex vertex) {
+        vertex.properties().forEachRemaining(vertexProperty -> {
+            PropertyKey pk = mgmt.getPropertyKey(vertexProperty.label());
+            if (null == pk) {
+                unknownPropertyKey(vertex, vertexProperty.label());
+            }
+
+            Object o = vertexProperty.value();
+
+            if (null == o) {
+                LOGGER.debug("Skipping key {} (null value)", vertexProperty.label());
+                return;
+            }
+
+            LOGGER.debug("Checking property {}", vertexProperty);
+
+            Cardinality card = pk.cardinality();
+
+            if (Cardinality.SINGLE == card && List.class.isAssignableFrom(o.getClass())) {
+                // List-valuedness is probably sufficient to throw an exception, but we'll allow a singleton here
+                List<?> l = (List<?>)o;
+                int size = l.size();
+                if (1 < size) {
+                    multipleValuesViolatesCardinalitySingle(vertex, vertexProperty.label(), l);
+                }
+            } else if (Cardinality.SET == card && Collection.class.isAssignableFrom(o.getClass())) {
+                // Check for duplicate values
+                Collection<Object> c = (Collection<Object>)o;
+                Set<Object> checkSet = new HashSet<>(c.size());
+                for (Object elem : c) {
+                    if (checkSet.contains(elem)) {
+                        multipleValuesViolatesCardinalitySet(vertex, vertexProperty.label(), elem);
+                    }
+                    checkSet.add(elem);
+                }
+            }
+        });
+    }
+
+    private void checkEdges(Vertex vertex) {
+
+        GraphTraversal<Vertex, Edge> gt = vertex.inE();
+
+        Set<String> simpleLabels = new HashSet<>();
+
+        while (gt.hasNext()) {
+            Edge e = gt.next();
+            String edgeLabelName = e.label();
+            EdgeLabel edgeLabel = mgmt.getEdgeLabel(edgeLabelName);
+
+            if (null == edgeLabel)
+                unknownEdgeLabel(vertex, edgeLabelName);
+
+            Multiplicity multi = edgeLabel.multiplicity();
+            if (multi == Multiplicity.ONE2MANY) {
+                long count = vertex.inE(edgeLabelName).count().next().longValue();
+                if (1 < count) {
+                    multipleEdgesViolatesOne2Many(vertex, edgeLabelName);
+                }
+            }
+
+            if (multi == Multiplicity.SIMPLE)
+                simpleLabels.add(edgeLabelName);
+        }
+
+        gt = vertex.outE();
+
+        while (gt.hasNext()) {
+            Edge e = gt.next();
+            String edgeLabelName = e.label();
+            EdgeLabel edgeLabel = mgmt.getEdgeLabel(edgeLabelName);
+            Multiplicity multi = edgeLabel.multiplicity();
+            if (multi == Multiplicity.MANY2ONE) {
+                long count = vertex.outE(edgeLabelName).count().next().longValue();
+                if (1 < count) {
+                    multipleEdgesViolateMany2One(vertex, edgeLabelName);
+                }
+            }
+
+            if (multi == Multiplicity.SIMPLE)
+                simpleLabels.add(edgeLabelName);
+        }
+
+        for (String simpleLabel : simpleLabels) {
+            gt = vertex.bothE(simpleLabel);
+
+            HashMap<Vertex, Vertex> simpleMap = new HashMap<>();
+
+            while (gt.hasNext()) {
+                Edge e = gt.next();
+                Vertex outV = e.outV().next();
+                Vertex inV = e.inV().next();
+
+                Vertex otherV = outV.equals(vertex) ? inV : outV;
+
+                if (otherV.equals(simpleMap.get(vertex)))
+                    multipleEdgesViolateSimple(vertex, simpleLabel);
+
+                simpleMap.put(vertex, otherV);
+            }
+        }
+
+        // TODO edge props?
+    }
+
+    private void unknownPropertyKey(Vertex v, String key) {
+        throw new IllegalArgumentException(String.format("Vertex %s has unknown property key %s", v, key));
+    }
+
+    private void unknownEdgeLabel(Vertex v, String label) {
+        throw new IllegalArgumentException(String.format("Vertex %s has an incident edge with unknown label %s", v, label));
+    }
+
+    private void multipleValuesViolatesCardinalitySingle(Vertex v, String key, List<?> valueList) {
+        String msg = String.format(
+                "Vertex %s's property key %s with value list %s violates cardinality constraint %s",
+                v, key, valueList, Cardinality.SINGLE);
+        throw new IllegalArgumentException(msg);
+    }
+
+    private void multipleValuesViolatesCardinalitySet(Vertex v, String key, Object duplicateElem) {
+        String msg = String.format(
+                "Vertex %s's property key %s violates %s constraint: multiple occurrences of value %s found",
+                v, key, Cardinality.SET, duplicateElem);
+        throw new IllegalArgumentException(msg);
+    }
+
+    private void multipleEdgesViolateSimple(Vertex vertex, String simpleLabel) {
+        String msg = String.format(
+                "Vertex %s has multiple edges with label %s; this violates the label's multiplicity %s",
+                vertex, simpleLabel, Multiplicity.SIMPLE);
+        throw new IllegalArgumentException(msg);
+    }
+
+    private void multipleEdgesViolateMany2One(Vertex vertex, String edgeLabelName) {
+        String msg = String.format(
+                "Vertex %s has multiple edges with label %s that violate the label's multiplicity constraint %s",
+                vertex, edgeLabelName, Multiplicity.MANY2ONE);
+        throw new IllegalArgumentException(msg);
+    }
+
+    private void multipleEdgesViolatesOne2Many(Vertex vertex, String edgeLabelName) {
+        String msg = String.format(
+                "Vertex %s has multiple edges with label %s that violate the label's multiplicity constraint %s",
+                vertex, edgeLabelName, Multiplicity.ONE2MANY);
+        throw new IllegalArgumentException(msg);
     }
 
     @Override
@@ -170,7 +347,7 @@ public class BulkLoaderVertexProgram implements VertexProgram<Long[]> {
             try {
                 final Properties properties = new Properties();
                 properties.load(new FileInputStream(propertiesFileLocation));
-                properties.forEach((key, value) -> this.configuration.setProperty(CONFIGURATION_PREFIX + key, value));
+                properties.forEach((key, value) -> this.configuration.setProperty(CFG_GRAPH_PREFIX + key, value));
                 return this;
             } catch (final Exception e) {
                 throw new IllegalArgumentException(e.getMessage(), e);
