JanusGraph/janusgraph

commit e68efba52332c501cd4fbdde93c948709dbe040c

Added GeoJson serialization for graphson https://github.com/thinkaurelius/titan/issues/954

titan-core/src/main/java/com/thinkaurelius/titan/core/attribute/Geoshape.java
@@ -1,5 +1,9 @@
 package com.thinkaurelius.titan.core.attribute;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.google.common.base.Preconditions;
 import com.google.common.primitives.Doubles;
 import com.spatial4j.core.context.SpatialContext;
@@ -11,6 +15,7 @@ import com.thinkaurelius.titan.diskstorage.WriteBuffer;
 import com.thinkaurelius.titan.graphdb.database.idhandling.VariableLong;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -498,6 +503,75 @@ public class Geoshape {
                     buffer.putFloat(coordinates[i][j]);
                 }
             }
+        }
+    }
+
+    /**
+     * @author Bryn Cooke
+     */
+    public static class GeoshapeGsonSerializer extends StdSerializer<Geoshape> {
+
+        public GeoshapeGsonSerializer(Class<Geoshape> t) {
+            super(t);
+        }
+
+        @Override
+        public void serialize(Geoshape value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+            jgen.writeStartObject();
+            jgen.writeFieldName("type");
+
+
+            switch(value.getType()) {
+                case POLYGON:
+                    throw new UnsupportedOperationException("Ploygons are not supported");
+                case BOX:
+                    jgen.writeString("Polygon");
+                    jgen.writeFieldName("coordinates");
+                    jgen.writeStartArray();
+
+                    jgen.writeStartArray();
+                    jgen.writeNumber(value.coordinates[1][0]);
+                    jgen.writeNumber(value.coordinates[0][0]);
+                    jgen.writeEndArray();
+
+                    jgen.writeStartArray();
+                    jgen.writeNumber(value.coordinates[1][1]);
+                    jgen.writeNumber(value.coordinates[0][0]);
+                    jgen.writeEndArray();
+
+                    jgen.writeStartArray();
+                    jgen.writeNumber(value.coordinates[1][1]);
+                    jgen.writeNumber(value.coordinates[0][1]);
+                    jgen.writeEndArray();
+
+                    jgen.writeStartArray();
+                    jgen.writeNumber(value.coordinates[1][0]);
+                    jgen.writeNumber(value.coordinates[0][1]);
+                    jgen.writeEndArray();
+
+                    jgen.writeEndArray();
+                    break;
+                case CIRCLE:
+                    jgen.writeString("Circle");
+                    jgen.writeFieldName("radius");
+                    jgen.writeNumber(value.getRadius());
+                    jgen.writeFieldName("coordinates");
+                    jgen.writeStartArray();
+                    jgen.writeNumber(value.coordinates[1][0]);
+                    jgen.writeNumber(value.coordinates[0][0]);
+                    jgen.writeEndArray();
+                    break;
+                case POINT:
+                    jgen.writeString("Point");
+                    jgen.writeFieldName("coordinates");
+                    jgen.writeStartArray();
+                    jgen.writeNumber(value.coordinates[1][0]);
+                    jgen.writeNumber(value.coordinates[0][0]);
+                    jgen.writeEndArray();
+                    break;
+            }
+            jgen.writeEndObject();
+
         }
     }
 }

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/database/StandardTitanGraph.java
@@ -1,12 +1,14 @@
 package com.thinkaurelius.titan.graphdb.database;
 
 import com.carrotsearch.hppc.LongArrayList;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 import com.google.common.collect.*;
 import com.thinkaurelius.titan.core.*;
 import com.thinkaurelius.titan.core.Cardinality;
+import com.thinkaurelius.titan.core.attribute.Geoshape;
 import com.thinkaurelius.titan.core.schema.*;
 import com.thinkaurelius.titan.core.Multiplicity;
 import com.thinkaurelius.titan.diskstorage.configuration.Configuration;
@@ -67,6 +69,9 @@ import com.thinkaurelius.titan.graphdb.vertices.StandardVertex;
 import org.apache.tinkerpop.gremlin.process.traversal.TraversalStrategies;
 import org.apache.tinkerpop.gremlin.structure.*;
 import com.thinkaurelius.titan.util.system.TXUtils;
+import org.apache.tinkerpop.gremlin.structure.io.DefaultIo;
+import org.apache.tinkerpop.gremlin.structure.io.graphson.GraphSONMapper;
+import org.apache.tinkerpop.gremlin.structure.io.graphson.GraphSONWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -805,5 +810,17 @@ public class StandardTitanGraph extends TitanBlueprintsGraph {
 
             graph.closeInternal();
         }
+    }
+
+    @Override
+    public Io io() {
+        return new DefaultIo(this) {
+            public GraphSONWriter.Builder graphSONWriter() {
+                SimpleModule module = new SimpleModule();
+                module.addSerializer(Geoshape.class, new Geoshape.GeoshapeGsonSerializer(Geoshape.class));
+                GraphSONMapper mapper = GraphSONMapper.build().addCustomModule(module).create();
+                return GraphSONWriter.build().mapper(mapper);
+            }
+        };
     }
 }

titan-test/src/test/java/com/thinkaurelius/titan/graphdb/attribute/GeoshapeTest.java
@@ -1,7 +1,8 @@
 package com.thinkaurelius.titan.graphdb.attribute;
 
-import com.fasterxml.jackson.core.JsonParseException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.thinkaurelius.titan.core.attribute.Geoshape;
 import org.junit.Test;
 
@@ -214,6 +215,17 @@ public class GeoshapeTest {
     }
 
 
+    @Test
+    public void testGeoJsonSerialization() throws IOException {
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(new Geoshape.GeoshapeGsonSerializer(Geoshape.class));
+        final ObjectMapper om = new ObjectMapper();
+        om.registerModule(module);
+        assertEquals("{\"type\":\"Point\",\"coordinates\":[20.5,10.5]}", om.writeValueAsString(Geoshape.point(10.5, 20.5)));
+        assertEquals("{\"type\":\"Polygon\",\"coordinates\":[[20.5,10.5],[22.5,10.5],[22.5,12.5],[20.5,12.5]]}", om.writeValueAsString(Geoshape.box(10.5, 20.5, 12.5, 22.5)));
+        assertEquals("{\"type\":\"Circle\",\"radius\":30.5,\"coordinates\":[20.5,10.5]}", om.writeValueAsString(Geoshape.circle(10.5, 20.5, 30.5)));
+
+    }
 
 
 }

commit 0da609c49204b3072aa594041dcd885df1734196

Fix issue introduced by https://github.com/thinkaurelius/titan/commit/e68efba52332c501cd4fbdde93c948709dbe040c where GeoGson serialisation support was overriding the default TitanIO.

titan-core/src/main/java/com/thinkaurelius/titan/core/attribute/Geoshape.java
@@ -511,8 +511,8 @@ public class Geoshape {
      */
     public static class GeoshapeGsonSerializer extends StdSerializer<Geoshape> {
 
-        public GeoshapeGsonSerializer(Class<Geoshape> t) {
-            super(t);
+        public GeoshapeGsonSerializer() {
+            super(Geoshape.class);
         }
 
         @Override

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/database/StandardTitanGraph.java
@@ -825,20 +825,6 @@ public class StandardTitanGraph extends TitanBlueprintsGraph {
         }
     }
 
-    @Override
-    public Io io() {
-        return new DefaultIo(this) {
-            public GraphSONWriter.Builder graphSONWriter() {
-                SimpleModule module = new SimpleModule();
-                module.addSerializer(Geoshape.class, new Geoshape.GeoshapeGsonSerializer(Geoshape.class));
-                GraphSONMapper mapper = GraphSONMapper.build().addCustomModule(module).create();
-                return GraphSONWriter.build().mapper(mapper);
-            }
-        };
-    }
-
-
-
     public static Graph empty() {
         return EMPTY;
     }

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/tinkerpop/io/graphson/TitanGraphSONModule.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.thinkaurelius.titan.core.attribute.Geoshape;
 import com.thinkaurelius.titan.graphdb.relations.RelationIdentifier;
 
 import java.io.IOException;
@@ -18,6 +19,7 @@ public class TitanGraphSONModule extends SimpleModule {
 
     private TitanGraphSONModule() {
         addSerializer(RelationIdentifier.class, new RelationIdentifierSerializer());
+        addSerializer(Geoshape.class, new Geoshape.GeoshapeGsonSerializer());
     }
 
     private static final TitanGraphSONModule INSTANCE = new TitanGraphSONModule();

titan-test/src/test/java/com/thinkaurelius/titan/graphdb/attribute/GeoshapeTest.java
@@ -218,7 +218,7 @@ public class GeoshapeTest {
     @Test
     public void testGeoJsonSerialization() throws IOException {
         SimpleModule module = new SimpleModule();
-        module.addSerializer(new Geoshape.GeoshapeGsonSerializer(Geoshape.class));
+        module.addSerializer(new Geoshape.GeoshapeGsonSerializer());
         final ObjectMapper om = new ObjectMapper();
         om.registerModule(module);
         assertEquals("{\"type\":\"Point\",\"coordinates\":[20.5,10.5]}", om.writeValueAsString(Geoshape.point(10.5, 20.5)));