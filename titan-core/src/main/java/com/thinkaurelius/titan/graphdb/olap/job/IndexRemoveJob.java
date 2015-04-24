0c291e18b7d1e81547d8ee63dc018610d2c7641d
Dan LaRocque <dalaro@hopcount.org>
2015-04-24 16:14:54 -0400

    Revert exception catch in IndexRemoveJob keyFilter
    This reverts commit 98f864b40c1970dce3f8a82e3fd16180343a50fd.

--- titan-core/src/main/java/com/thinkaurelius/titan/graphdb/olap/job/IndexRemoveJob.java
+++ titan-core/src/main/java/com/thinkaurelius/titan/graphdb/olap/job/IndexRemoveJob.java
@@ -160,14 +160,7 @@ public class IndexRemoveJob extends IndexUpdateJob implements ScanJob {
     public Predicate<StaticBuffer> getKeyFilter() {
         if (isGlobalGraphIndex()) {
             assert graphIndexId>0;
-            return (k -> {
-                try {
-                    return indexSerializer.getIndexIdFromKey(k) == graphIndexId;
-                } catch (RuntimeException e) {
-                    log.debug("Filtering key {} due to exception", k, e);
-                    return false;
-                }
-            });
+            return (k -> indexSerializer.getIndexIdFromKey(k)==graphIndexId);
         } else {
             return buffer -> {
                 long vertexId = idManager.getKeyID(buffer);
