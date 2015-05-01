8f733f970068fca46732ca2f40350c908aab45c8
Dan LaRocque <dalaro@hopcount.org>
2015-05-01 01:00:11 -0400

    Increase out-of-bounds exception specificity
    StaticArrayBuffer checked position<0 || base+size>limit and threw an
    exception if the expression was false.  This commit breaks each half
    of the disjunction into a separate check with a distinct exception.
    Also added a bit more info to the string in the base+size>limit case.

--- titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/util/StaticArrayBuffer.java
+++ titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/util/StaticArrayBuffer.java
@@ -74,7 +74,11 @@ public class StaticArrayBuffer implements StaticBuffer {
 
     private int require(int position, int size) {
         int base = position + offset;
-        if (position<0 || base+size>limit) throw new ArrayIndexOutOfBoundsException("Position ["+position+"] and or size ["+size+"] out of bounds");
+        if (position<0)
+            throw new ArrayIndexOutOfBoundsException("Position [" + position + "] must be nonnegative");
+        if (base+size>limit)
+            throw new ArrayIndexOutOfBoundsException("Required size [" + size + "] " +
+                    "exceeds actual remaining size [" + (limit-base) + "]");
         assert base + size <= limit;
         return base;
     }
