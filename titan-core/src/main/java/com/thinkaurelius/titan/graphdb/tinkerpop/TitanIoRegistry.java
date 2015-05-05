b7694f37a5f51f42894e93ade3457797a594b1ec
Matthias Broecheler <me@matthiasb.com>
2015-05-04 18:49:06 -0700

    Updated to TP3 M9rc1 and attempted to fix all compile errors. Tests have not yet been run.

--- titan-core/src/main/java/com/thinkaurelius/titan/graphdb/tinkerpop/TitanIoRegistry.java
+++ titan-core/src/main/java/com/thinkaurelius/titan/graphdb/tinkerpop/TitanIoRegistry.java
@@ -4,7 +4,7 @@ import com.thinkaurelius.titan.core.TitanGraph;
 import com.thinkaurelius.titan.graphdb.database.StandardTitanGraph;
 import com.thinkaurelius.titan.graphdb.relations.RelationIdentifier;
 import com.thinkaurelius.titan.graphdb.tinkerpop.io.graphson.TitanGraphSONModule;
-import org.apache.tinkerpop.gremlin.structure.io.DefaultIo;
+import org.apache.tinkerpop.gremlin.structure.io.IoCore;
 import org.apache.tinkerpop.gremlin.structure.io.graphson.GraphSONMapper;
 import org.apache.tinkerpop.gremlin.structure.io.gryo.GryoMapper;
 
@@ -13,19 +13,19 @@ import org.apache.tinkerpop.gremlin.structure.io.gryo.GryoMapper;
  *
  * @author Matthias Broecheler (me@matthiasb.com)
  */
-public class TitanIo extends DefaultIo {
+public class TitanIoRegistry {
 
-    public TitanIo(TitanGraph graph) {
-        super(graph);
+    public TitanIoRegistry(TitanGraph graph) {
+        //super(graph);
     }
 
-    @Override
+    //@Override
     public GryoMapper.Builder gryoMapper() {
         //Add user registered serializers
         return GryoMapper.build().addCustom(RelationIdentifier.class);
     }
 
-    @Override
+    //@Override
     public GraphSONMapper.Builder graphSONMapper() {
         return GraphSONMapper.build().addCustomModule(TitanGraphSONModule.getInstance());
     }
