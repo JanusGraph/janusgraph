thinkaurelius/titan

Update TitanGraphTest.java
get size of iterable

titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanGraphTest.java
@@ -1600,9 +1600,9 @@ public abstract class TitanGraphTest extends TitanGraphBaseTest {
 
         newTx();
 
-        assertEquals(0,tx.query().has("name","name1").vertices());
-        assertEquals(1,tx.query().has("name","name2").vertices());
-        assertEquals(1,tx.query().has("uid",512).vertices());
+        assertEquals(0,Iterables.size(tx.query().has("name","name1").vertices()));
+        assertEquals(1,Iterables.size(tx.query().has("name","name2").vertices()));
+        assertEquals(1,Iterables.size(tx.query().has("uid",512).vertices()));
 
     }
 

JanusGraph/janusgraph

get size of iterable(cherry picked from commit 9ee8acaebae17e8f4159d39c886992e55e4fd17b)
Conflicts:
	titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanGraphTest.java

titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanGraphTest.java
@@ -1574,9 +1574,9 @@ public abstract class TitanGraphTest extends TitanGraphBaseTest {
 
         newTx();
 
-        assertCount(0, tx.query().has("name", "name1").vertices());
-        assertCount(1, tx.query().has("name", "name2").vertices());
-        assertCount(1, tx.query().has("uid", 512).vertices());
+        assertCount(0, Iterables.size(tx.query().has("name", "name1").vertices()));
+        assertCount(1, Iterables.size(tx.query().has("name", "name2").vertices()));
+        assertCount(1, Iterables.size(tx.query().has("uid", 512).vertices()));
     }
 
     /**