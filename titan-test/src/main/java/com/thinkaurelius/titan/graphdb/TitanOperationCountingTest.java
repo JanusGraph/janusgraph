thinkaurelius/titan

Added test case for #931

titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanOperationCountingTest.java
@@ -217,7 +217,7 @@ public abstract class TitanOperationCountingTest extends TitanGraphBaseTest {
         tx.commit();
 
 
-        TitanTransaction tx = graph.buildTransaction().checkExternalVertexExistence(false).setGroupName(metricsPrefix).start();
+        TitanTransaction tx = graph.buildTransaction().checkExternalVertexExistence(false).consistencyChecks(false).setGroupName(metricsPrefix).start();
         v = tx.getVertex(v.getLongId());
         v.setProperty("foo","bus");
         tx.commit();
@@ -226,6 +226,16 @@ public abstract class TitanOperationCountingTest extends TitanGraphBaseTest {
         verifyStoreMetrics(STORE_NAMES.get(1));
         verifyStoreMetrics(STORE_NAMES.get(2));
         verifyStoreMetrics(STORE_NAMES.get(3), ImmutableMap.of(M_MUTATE, 1l));
+
+        tx = graph.buildTransaction().setGroupName(metricsPrefix).start();
+        v = tx.getVertex(v.getLongId());
+        v.setProperty("foo","bus");
+        tx.commit();
+        printAllMetrics();
+        verifyStoreMetrics(STORE_NAMES.get(0), ImmutableMap.of(M_GET_SLICE, 2l));
+        verifyStoreMetrics(STORE_NAMES.get(1));
+        verifyStoreMetrics(STORE_NAMES.get(2));
+        verifyStoreMetrics(STORE_NAMES.get(3), ImmutableMap.of(M_MUTATE, 2l));
     }
 
 

JanusGraph/janusgraph

@dalaro: This commit fixes a test failure introduced by
278d8b988058183ed032b03959771864f6b37a0c (cherry-picked into titan09
as 49732814d645303fae0a6b2dd2929f642d9a2347).  Without this commit,
the test fails on both titan05 and titan09.
(cherry picked from commit 41fa56113a19a58a4b93cc15c7ad386b98844595)
Conflicts:
	titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanOperationCountingTest.java

titan-test/src/main/java/com/thinkaurelius/titan/graphdb/TitanOperationCountingTest.java
@@ -219,7 +219,7 @@ public abstract class TitanOperationCountingTest extends TitanGraphBaseTest {
         tx.commit();
 
 
-        TitanTransaction tx = graph.buildTransaction().checkExternalVertexExistence(false).groupName(metricsPrefix).start();
+        TitanTransaction tx = graph.buildTransaction().checkExternalVertexExistence(false).consistencyChecks(false).groupName(metricsPrefix).start();
         v = tx.getVertex(v.longId());
         v.property("foo","bus");
         tx.commit();
@@ -228,6 +228,16 @@ public abstract class TitanOperationCountingTest extends TitanGraphBaseTest {
         verifyStoreMetrics(STORE_NAMES.get(1));
         verifyStoreMetrics(STORE_NAMES.get(2));
         verifyStoreMetrics(STORE_NAMES.get(3), ImmutableMap.of(M_MUTATE, 1l));
+
+        tx = graph.buildTransaction().groupName(metricsPrefix).start();
+        v = tx.getVertex(v.longId());
+        v.property("foo","bus");
+        tx.commit();
+        printAllMetrics();
+        verifyStoreMetrics(STORE_NAMES.get(0), ImmutableMap.of(M_GET_SLICE, 2l));
+        verifyStoreMetrics(STORE_NAMES.get(1));
+        verifyStoreMetrics(STORE_NAMES.get(2));
+        verifyStoreMetrics(STORE_NAMES.get(3), ImmutableMap.of(M_MUTATE, 2l));
     }
 
 