JanusGraph/janusgraph

commit fab0f61a025f824f76980d54c3aa2cfa435a8c79

Bulkloader and Hadoop IO format config key changes
BulkLoaderVertexProgram and the Hadoop Input/OutputFormats now use
harmonized config key prefixes: titanmr.{ioformat,bulkload}.conf.
Also added a ConfigElement.getPath overload that includes the root
element name (the default behavior is still to exclude the root).

This commit fixes some DEBUG logging statements needlessly emitted at
ERROR.

For #1045

titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/MapReduceIndexJobs.java
@@ -147,7 +147,7 @@ public class MapReduceIndexJobs {
         for (Map.Entry<Object, Object> e : source.entrySet()) {
             String k;
             String v = e.getValue().toString();
-            k = ConfigElement.getPath(TitanHadoopConfiguration.TITAN_INPUT_CONFIG_KEYS) + "." + e.getKey().toString();
+            k = ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + e.getKey().toString();
             sink.set(k, v);
             log.info("Set {}={}", k, v);
         }

titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/MapReduceIndexManagement.java
@@ -161,7 +161,7 @@ public class MapReduceIndexManagement {
         copyIndexJobKeys(hadoopConf, indexName, relationTypeName);
         titanmrConf.set(TitanHadoopConfiguration.SCAN_JOB_CONFIG_ROOT,
                 GraphDatabaseConfiguration.class.getName() + "#JOB_NS");
-        // Copy the StandardTitanGraph configuration under TitanHadoopConfiguration.TITAN_INPUT_CONFIG_KEYS
+        // Copy the StandardTitanGraph configuration under TitanHadoopConfiguration.GRAPH_CONFIG_KEYS
         org.apache.commons.configuration.Configuration localbc = graph.getConfiguration().getLocalConfiguration();
         localbc.clearProperty(Graph.GRAPH);
         copyInputKeys(hadoopConf, localbc);
@@ -191,7 +191,7 @@ public class MapReduceIndexManagement {
             if (!pid.element.isOption())
                 continue;
 
-            String k = ConfigElement.getPath(TitanHadoopConfiguration.TITAN_INPUT_CONFIG_KEYS) + "." + key;
+            String k = ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + key;
             String v = source.getProperty(key).toString();
 
             hadoopConf.set(k, v);

titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/scan/CassandraHadoopScanRunner.java
@@ -45,8 +45,9 @@ public class CassandraHadoopScanRunner extends AbstractHadoopScanRunner<Cassandr
 
         if (null != titanConf) {
             for (String k : titanConf.getKeys("")) {
-                hadoopConf.set(ConfigElement.getPath(TitanHadoopConfiguration.TITAN_INPUT_CONFIG_KEYS) + "." + k, titanConf.<Object>get(k, Object.class).toString());
-                log.error("Set: {}={}", ConfigElement.getPath(TitanHadoopConfiguration.TITAN_INPUT_CONFIG_KEYS) + "." + k, titanConf.<Object>get(k, Object.class).toString());
+                hadoopConf.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + k, titanConf.<Object>get(k, Object.class).toString());
+                log.debug("Set: {}={}", ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + k,
+                        titanConf.<Object>get(k, Object.class).toString());
             }
         }
 

titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/scan/HBaseHadoopScanRunner.java
@@ -37,8 +37,9 @@ public class HBaseHadoopScanRunner extends AbstractHadoopScanRunner<HBaseHadoopS
 
         if (null != titanConf) {
             for (String k : titanConf.getKeys("")) {
-                hadoopConf.set(ConfigElement.getPath(TitanHadoopConfiguration.TITAN_INPUT_CONFIG_KEYS) + "." + k, titanConf.<Object>get(k, Object.class).toString());
-                log.error("Set: {}={}", ConfigElement.getPath(TitanHadoopConfiguration.TITAN_INPUT_CONFIG_KEYS) + "." + k, titanConf.<Object>get(k, Object.class).toString());
+                hadoopConf.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + k, titanConf.<Object>get(k, Object.class).toString());
+                log.debug("Set: {}={}", ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + k,
+                        titanConf.<Object>get(k, Object.class).toString());
             }
         }
         Preconditions.checkNotNull(hadoopConf);

titan-hadoop-parent/titan-hadoop-core/src/test/java/com/thinkaurelius/titan/hadoop/CassandraScanJobIT.java
@@ -85,8 +85,8 @@ public class CassandraScanJobIT extends TitanGraphBaseTest {
         graph.tx().commit();
 
         org.apache.hadoop.conf.Configuration c = new org.apache.hadoop.conf.Configuration();
-        c.set(ConfigElement.getPath(TitanHadoopConfiguration.TITAN_INPUT_CONFIG_KEYS) + "." + "storage.cassandra.keyspace", getClass().getSimpleName());
-        c.set(ConfigElement.getPath(TitanHadoopConfiguration.TITAN_INPUT_CONFIG_KEYS) + "." + "storage.backend", "cassandrathrift");
+        c.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + "storage.cassandra.keyspace", getClass().getSimpleName());
+        c.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + "storage.backend", "cassandrathrift");
         c.set("cassandra.input.partitioner.class", "org.apache.cassandra.dht.ByteOrderedPartitioner");
 
         Job job = getVertexJobWithDefaultMapper(c);
@@ -113,8 +113,8 @@ public class CassandraScanJobIT extends TitanGraphBaseTest {
         graph.tx().commit();
 
         org.apache.hadoop.conf.Configuration c = new org.apache.hadoop.conf.Configuration();
-        c.set(ConfigElement.getPath(TitanHadoopConfiguration.TITAN_INPUT_CONFIG_KEYS) + "." + "storage.cassandra.keyspace", getClass().getSimpleName());
-        c.set(ConfigElement.getPath(TitanHadoopConfiguration.TITAN_INPUT_CONFIG_KEYS) + "." + "storage.backend", "cassandrathrift");
+        c.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + "storage.cassandra.keyspace", getClass().getSimpleName());
+        c.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + "storage.backend", "cassandrathrift");
         c.set(ConfigElement.getPath(TitanHadoopConfiguration.FILTER_PARTITIONED_VERTICES), "true");
         c.set("cassandra.input.partitioner.class", "org.apache.cassandra.dht.ByteOrderedPartitioner");
 

titan-core/src/main/java/com/thinkaurelius/titan/diskstorage/configuration/ConfigElement.java
@@ -131,6 +131,10 @@ public abstract class ConfigElement {
 //    }
 
     public static String getPath(ConfigElement element, String... umbrellaElements) {
+        return getPath(element, false, umbrellaElements);
+    }
+
+    public static String getPath(ConfigElement element, boolean includeRoot, String... umbrellaElements) {
         Preconditions.checkNotNull(element);
         if (umbrellaElements==null) umbrellaElements = new String[0];
         String path = element.getName();
@@ -146,6 +150,13 @@ public abstract class ConfigElement {
             }
             path = parent.getName() + SEPARATOR + path;
             element = parent;
+        }
+        if (includeRoot) {
+            // Assumes that roots are not umbrellas
+            // If roots could be umbrellas, we might have to change the interpretation of umbrellaElements
+            path = (element.isNamespace() ?
+                    element.getName() :
+                    element.getNamespace().getName()) + SEPARATOR + path;
         }
         //Don't make this check so that we can still access more general config items
         Preconditions.checkArgument(umbrellaPos<0,"Found unused umbrella element: %s",umbrellaPos<0?null:umbrellaElements[umbrellaPos]);

titan-core/src/main/java/com/thinkaurelius/titan/graphdb/tinkerpop/computer/bulkloader/BulkLoaderVertexProgram.java
@@ -40,8 +40,8 @@ public class BulkLoaderVertexProgram implements VertexProgram<long[]> {
     private static final Logger LOGGER = LoggerFactory.getLogger(BulkLoaderVertexProgram.class);
 
     // TODO this is a step backwards in config management
-    private static final String CFG_GRAPH_PREFIX = "titan.bulkload.graphconfig.";
-    private static final String CFG_SCHEMA_CHECK = "titan.bulkload.schemacheck";
+    private static final String CFG_GRAPH_PREFIX = "titanmr.bulkload.conf.";
+    private static final String CFG_SCHEMA_CHECK = "titanmr.bulkload.schema-check";
     private static final boolean CFG_SCHEMA_CHECK_DEFAULT = false;
 
     private static final String TITAN_ID = "titan.id";
@@ -337,11 +337,9 @@ public class BulkLoaderVertexProgram implements VertexProgram<long[]> {
         return this;
     }
 
-    // TODO verify ResultGraph.ORIGINAL & Persist.EDGES trigger intended behavior
-
     @Override
     public GraphComputer.ResultGraph getPreferredResultGraph() {
-        return GraphComputer.ResultGraph.ORIGINAL;
+        return GraphComputer.ResultGraph.NEW;
     }
 
     @Override

titan-hadoop-parent/titan-hadoop-1/src/main/java/com/thinkaurelius/titan/hadoop/formats/TitanH1OutputFormat.java
@@ -44,7 +44,7 @@ public class TitanH1OutputFormat extends OutputFormat<NullWritable, VertexWritab
                 Configuration hadoopConf = taskAttemptContext.getConfiguration();
                 ModifiableHadoopConfiguration mhc =
                         ModifiableHadoopConfiguration.of(TitanHadoopConfiguration.MAPRED_NS, hadoopConf);
-                graph = (StandardTitanGraph) TitanFactory.open(mhc.getTitanInputConf());
+                graph = (StandardTitanGraph) TitanFactory.open(mhc.getTitanGraphConf());
             }
         }
 

commit c45960b3fafc0bb89d8e94ddb9cf8c70d035ea46

Fix Hadoop config namespace refs
This commit fixes a bug introduced by
fab0f61a025f824f76980d54c3aa2cfa435a8c79.  I missed some config
namespace references in that commit that I should have updated,
breaking some scanjob helper classes.

titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/MapReduceIndexJobs.java
@@ -144,10 +144,11 @@ public class MapReduceIndexJobs {
     }
 
     private static void copyPropertiesToInputAndOutputConf(org.apache.hadoop.conf.Configuration sink, Properties source) {
+        final String prefix = ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS, true) + ".";
         for (Map.Entry<Object, Object> e : source.entrySet()) {
             String k;
             String v = e.getValue().toString();
-            k = ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + e.getKey().toString();
+            k = prefix + e.getKey().toString();
             sink.set(k, v);
             log.info("Set {}={}", k, v);
         }

titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/MapReduceIndexManagement.java
@@ -191,7 +191,7 @@ public class MapReduceIndexManagement {
             if (!pid.element.isOption())
                 continue;
 
-            String k = ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + key;
+            String k = ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS, true) + "." + key;
             String v = source.getProperty(key).toString();
 
             hadoopConf.set(k, v);
@@ -200,13 +200,13 @@ public class MapReduceIndexManagement {
     }
 
     private static void copyIndexJobKeys(org.apache.hadoop.conf.Configuration hadoopConf, String indexName, String relationType) {
-        hadoopConf.set(ConfigElement.getPath(TitanHadoopConfiguration.SCAN_JOB_CONFIG_KEYS) + "." +
+        hadoopConf.set(ConfigElement.getPath(TitanHadoopConfiguration.SCAN_JOB_CONFIG_KEYS, true) + "." +
                         ConfigElement.getPath(IndexUpdateJob.INDEX_NAME), indexName);
 
-        hadoopConf.set(ConfigElement.getPath(TitanHadoopConfiguration.SCAN_JOB_CONFIG_KEYS) + "." +
+        hadoopConf.set(ConfigElement.getPath(TitanHadoopConfiguration.SCAN_JOB_CONFIG_KEYS, true) + "." +
                 ConfigElement.getPath(IndexUpdateJob.INDEX_RELATION_TYPE), relationType);
 
-        hadoopConf.set(ConfigElement.getPath(TitanHadoopConfiguration.SCAN_JOB_CONFIG_KEYS) + "." +
+        hadoopConf.set(ConfigElement.getPath(TitanHadoopConfiguration.SCAN_JOB_CONFIG_KEYS, true) + "." +
                 ConfigElement.getPath(GraphDatabaseConfiguration.JOB_START_TIME),
                 String.valueOf(System.currentTimeMillis()));
     }

titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/scan/CassandraHadoopScanRunner.java
@@ -45,8 +45,9 @@ public class CassandraHadoopScanRunner extends AbstractHadoopScanRunner<Cassandr
 
         if (null != titanConf) {
             for (String k : titanConf.getKeys("")) {
-                hadoopConf.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + k, titanConf.<Object>get(k, Object.class).toString());
-                log.debug("Set: {}={}", ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + k,
+                String prefix = ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS, true) + ".";
+                hadoopConf.set(prefix + k, titanConf.get(k, Object.class).toString());
+                log.debug("Set: {}={}", prefix + k,
                         titanConf.<Object>get(k, Object.class).toString());
             }
         }

titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/scan/HBaseHadoopScanRunner.java
@@ -36,10 +36,10 @@ public class HBaseHadoopScanRunner extends AbstractHadoopScanRunner<HBaseHadoopS
                 baseHadoopConf : new org.apache.hadoop.conf.Configuration();
 
         if (null != titanConf) {
+            String prefix = ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS, true) + ".";
             for (String k : titanConf.getKeys("")) {
-                hadoopConf.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + k, titanConf.<Object>get(k, Object.class).toString());
-                log.debug("Set: {}={}", ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + k,
-                        titanConf.<Object>get(k, Object.class).toString());
+                hadoopConf.set(prefix + k, titanConf.get(k, Object.class).toString());
+                log.debug("Set: {}={}", prefix + k, titanConf.get(k, Object.class).toString());
             }
         }
         Preconditions.checkNotNull(hadoopConf);

titan-hadoop-parent/titan-hadoop-core/src/test/java/com/thinkaurelius/titan/hadoop/CassandraScanJobIT.java
@@ -85,8 +85,8 @@ public class CassandraScanJobIT extends TitanGraphBaseTest {
         graph.tx().commit();
 
         org.apache.hadoop.conf.Configuration c = new org.apache.hadoop.conf.Configuration();
-        c.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + "storage.cassandra.keyspace", getClass().getSimpleName());
-        c.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + "storage.backend", "cassandrathrift");
+        c.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS, true) + "." + "storage.cassandra.keyspace", getClass().getSimpleName());
+        c.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS, true) + "." + "storage.backend", "cassandrathrift");
         c.set("cassandra.input.partitioner.class", "org.apache.cassandra.dht.ByteOrderedPartitioner");
 
         Job job = getVertexJobWithDefaultMapper(c);
@@ -113,8 +113,8 @@ public class CassandraScanJobIT extends TitanGraphBaseTest {
         graph.tx().commit();
 
         org.apache.hadoop.conf.Configuration c = new org.apache.hadoop.conf.Configuration();
-        c.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + "storage.cassandra.keyspace", getClass().getSimpleName());
-        c.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS) + "." + "storage.backend", "cassandrathrift");
+        c.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS, true) + "." + "storage.cassandra.keyspace", getClass().getSimpleName());
+        c.set(ConfigElement.getPath(TitanHadoopConfiguration.GRAPH_CONFIG_KEYS, true) + "." + "storage.backend", "cassandrathrift");
         c.set(ConfigElement.getPath(TitanHadoopConfiguration.FILTER_PARTITIONED_VERTICES), "true");
         c.set("cassandra.input.partitioner.class", "org.apache.cassandra.dht.ByteOrderedPartitioner");
 