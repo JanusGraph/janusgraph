afffa0794213ce1094b66668d2d4aaafd534bc10
Dan LaRocque <dalaro@hopcount.org>
2015-05-07 06:42:02 -0400

    Rework Cassandra InputFormat for widerows support
    This supports widerows at the binary level.  This is a different
    approach than the one I took on titan05, where widerows was supported
    at the FaunusVertex/TinkerVertex level.  Pushing this down has
    somewhat simplified the logic.  VertexBuilder is no longer needed.
    
    For #1075

--- titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/formats/cassandra/CassandraBinaryRecordReader.java
+++ titan-hadoop-parent/titan-hadoop-core/src/main/java/com/thinkaurelius/titan/hadoop/formats/cassandra/CassandraBinaryRecordReader.java
@@ -13,7 +13,10 @@ import org.apache.hadoop.mapreduce.TaskAttemptContext;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
 
@@ -23,6 +26,9 @@ import java.util.SortedMap;
 public class CassandraBinaryRecordReader extends RecordReader<StaticBuffer, Iterable<Entry>> {
 
     private ColumnFamilyRecordReader reader;
+
+    private KV currentKV;
+    private KV incompleteKV;
 
     public CassandraBinaryRecordReader(final ColumnFamilyRecordReader reader) {
         this.reader = reader;
@@ -35,17 +41,57 @@ public class CassandraBinaryRecordReader extends RecordReader<StaticBuffer, Iter
 
     @Override
     public boolean nextKeyValue() throws IOException, InterruptedException {
-        return reader.nextKeyValue();
+        return null != (currentKV = completeNextKV());
+    }
+
+    private KV completeNextKV() throws IOException {
+        KV completedKV = null;
+        boolean hasNext;
+        do {
+            hasNext = reader.nextKeyValue();
+
+            if (!hasNext) {
+                completedKV = incompleteKV;
+                incompleteKV = null;
+            } else {
+                StaticArrayBuffer key = StaticArrayBuffer.of(reader.getCurrentKey());
+                SortedMap<ByteBuffer, Column> valueSortedMap = reader.getCurrentValue();
+                List<Entry> entries = new ArrayList<>(valueSortedMap.size());
+                for (Map.Entry<ByteBuffer, Column> ent : valueSortedMap.entrySet()) {
+                    ByteBuffer col = ent.getKey();
+                    ByteBuffer val = ent.getValue().value();
+                    entries.add(StaticArrayEntry.of(StaticArrayBuffer.of(col), StaticArrayBuffer.of(val)));
+                }
+
+                if (null == incompleteKV) {
+                    // Initialization; this should happen just once in an instance's lifetime
+                    incompleteKV = new KV(key);
+                } else if (!incompleteKV.key.equals(key)) {
+                    // The underlying Cassandra reader has just changed to a key we haven't seen yet
+                    // This implies that there will be no more entries for the prior key
+                    completedKV = incompleteKV;
+                    incompleteKV = new KV(key);
+                }
+
+                incompleteKV.addEntries(entries);
+            }
+            /* Loop ends when either
+             * A) the cassandra reader ran out of data
+             * or
+             * B) the cassandra reader switched keys, thereby completing a KV */
+        } while (hasNext && null == completedKV);
+
+        return completedKV;
     }
 
     @Override
     public StaticBuffer getCurrentKey() throws IOException, InterruptedException {
-        return StaticArrayBuffer.of(reader.getCurrentKey());
+        return currentKV.key;
     }
 
     @Override
     public Iterable<Entry> getCurrentValue() throws IOException, InterruptedException {
-        return new CassandraMapIterable(reader.getCurrentValue());
+        return currentKV.entries;
     }
 
     @Override
@@ -98,6 +144,22 @@ public class CassandraBinaryRecordReader extends RecordReader<StaticBuffer, Iter
         @Override
         public void remove() {
             throw new UnsupportedOperationException();
+        }
+    }
+
+    private static class KV {
+        private final StaticArrayBuffer key;
+        private ArrayList<Entry> entries;
+
+        public KV(StaticArrayBuffer key) {
+            this.key = key;
+        }
+
+        public void addEntries(Collection<Entry> toAdd) {
+            if (null == entries)
+                entries = new ArrayList<>(toAdd.size());
+
+            entries.addAll(toAdd);
         }
     }
 }
