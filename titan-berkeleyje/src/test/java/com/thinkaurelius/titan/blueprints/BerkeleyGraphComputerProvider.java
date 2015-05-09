6bbe7c5f557f519967705ccf11ab13f17a99ad5e
Marko A. Rodriguez <okrammarko@gmail.com>
2015-05-09 00:25:41 -0600

    well of course BerkeleyJETx.class needs to be registered. what a fool I am.

--- titan-berkeleyje/src/test/java/com/thinkaurelius/titan/blueprints/BerkeleyGraphComputerProvider.java
+++ titan-berkeleyje/src/test/java/com/thinkaurelius/titan/blueprints/BerkeleyGraphComputerProvider.java
@@ -2,10 +2,12 @@ package com.thinkaurelius.titan.blueprints;
 
 import com.thinkaurelius.titan.BerkeleyStorageSetup;
 import com.thinkaurelius.titan.StorageSetup;
+import com.thinkaurelius.titan.diskstorage.berkeleyje.BerkeleyJETx;
 import com.thinkaurelius.titan.diskstorage.configuration.ModifiableConfiguration;
 import com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration;
 
 import java.time.Duration;
+import java.util.Set;
 
 /**
  * @author Matthias Broecheler (me@matthiasb.com)
@@ -18,6 +20,13 @@ public class BerkeleyGraphComputerProvider extends AbstractTitanGraphComputerPro
         config.set(GraphDatabaseConfiguration.IDAUTHORITY_WAIT, Duration.ofMillis(20));
         config.set(GraphDatabaseConfiguration.STORAGE_TRANSACTIONAL,false);
         return config;
+    }
+
+    @Override
+    public Set<Class> getImplementations() {
+        final Set<Class> implementations = super.getImplementations();
+        implementations.add(BerkeleyJETx.class);
+        return implementations;
     }
 
 }
--- titan-berkeleyje/src/test/java/com/thinkaurelius/titan/blueprints/BerkeleyGraphProvider.java
+++ titan-berkeleyje/src/test/java/com/thinkaurelius/titan/blueprints/BerkeleyGraphProvider.java
@@ -2,10 +2,12 @@ package com.thinkaurelius.titan.blueprints;
 
 import com.thinkaurelius.titan.BerkeleyStorageSetup;
 import com.thinkaurelius.titan.StorageSetup;
+import com.thinkaurelius.titan.diskstorage.berkeleyje.BerkeleyJETx;
 import com.thinkaurelius.titan.diskstorage.configuration.ModifiableConfiguration;
 import com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration;
 
 import java.time.Duration;
+import java.util.Set;
 
 /**
  * @author Matthias Broecheler (me@matthiasb.com)
@@ -15,6 +17,13 @@ public class BerkeleyGraphProvider extends AbstractTitanGraphProvider {
     @Override
     public ModifiableConfiguration getTitanConfiguration(String graphName, Class<?> test, String testMethodName) {
         return BerkeleyStorageSetup.getBerkeleyJEConfiguration(StorageSetup.getHomeDir(graphName)).set(GraphDatabaseConfiguration.IDAUTHORITY_WAIT, Duration.ofMillis(20));
+    }
+
+    @Override
+    public Set<Class> getImplementations() {
+        final Set<Class> implementations = super.getImplementations();
+        implementations.add(BerkeleyJETx.class);
+        return implementations;
     }
 
 }
