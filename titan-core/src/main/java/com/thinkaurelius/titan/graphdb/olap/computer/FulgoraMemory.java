b5bc4e3b2a2154a830c5c56bdb6bad94a03af997
Marko A. Rodriguez <okrammarko@gmail.com>
2015-05-20 09:31:24 -0600

    Fulgora no longer extends the TinkerGraph MapReduce classes. Copy pasted over the code to Fulgora.

--- titan-core/src/main/java/com/thinkaurelius/titan/graphdb/olap/computer/FulgoraMemory.java
+++ titan-core/src/main/java/com/thinkaurelius/titan/graphdb/olap/computer/FulgoraMemory.java
@@ -1,26 +1,131 @@
 package com.thinkaurelius.titan.graphdb.olap.computer;
 
+import org.apache.tinkerpop.gremlin.process.computer.GraphComputer;
 import org.apache.tinkerpop.gremlin.process.computer.MapReduce;
+import org.apache.tinkerpop.gremlin.process.computer.Memory;
 import org.apache.tinkerpop.gremlin.process.computer.VertexProgram;
-import org.apache.tinkerpop.gremlin.tinkergraph.process.computer.TinkerMemory;
+import org.apache.tinkerpop.gremlin.process.computer.util.MemoryHelper;
+import org.apache.tinkerpop.gremlin.structure.util.StringFactory;
 
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
  * @author Matthias Broecheler (me@matthiasb.com)
  */
-public class FulgoraMemory extends TinkerMemory {
+public class FulgoraMemory implements Memory.Admin {
 
-    public FulgoraMemory(VertexProgram<?> vertexProgram, Set<MapReduce> mapReducers) {
-        super(vertexProgram, mapReducers);
+    public final Set<String> memoryKeys = new HashSet<>();
+    public Map<String, Object> previousMap;
+    public Map<String, Object> currentMap;
+    private final AtomicInteger iteration = new AtomicInteger(0);
+    private final AtomicLong runtime = new AtomicLong(0l);
+
+    public FulgoraMemory(final VertexProgram<?> vertexProgram, final Set<MapReduce> mapReducers) {
+        this.currentMap = new ConcurrentHashMap<>();
+        this.previousMap = new ConcurrentHashMap<>();
+        if (null != vertexProgram) {
+            for (final String key : vertexProgram.getMemoryComputeKeys()) {
+                MemoryHelper.validateKey(key);
+                this.memoryKeys.add(key);
+            }
+        }
+        for (final MapReduce mapReduce : mapReducers) {
+            this.memoryKeys.add(mapReduce.getMemoryKey());
+        }
     }
 
-    protected void completeSubRound() {
-        super.completeSubRound();
+    @Override
+    public Set<String> keys() {
+        return this.previousMap.keySet();
+    }
+
+    @Override
+    public void incrIteration() {
+        this.iteration.getAndIncrement();
+    }
+
+    @Override
+    public void setIteration(final int iteration) {
+        this.iteration.set(iteration);
+    }
+
+    @Override
+    public int getIteration() {
+        return this.iteration.get();
+    }
+
+    @Override
+    public void setRuntime(final long runTime) {
+        this.runtime.set(runTime);
+    }
+
+    @Override
+    public long getRuntime() {
+        return this.runtime.get();
     }
 
     protected void complete() {
-        super.complete();
+        this.iteration.decrementAndGet();
+        this.previousMap = this.currentMap;
     }
 
+    protected void completeSubRound() {
+        this.previousMap = new ConcurrentHashMap<>(this.currentMap);
+
+    }
+
+    @Override
+    public boolean isInitialIteration() {
+        return this.getIteration() == 0;
+    }
+
+    @Override
+    public <R> R get(final String key) throws IllegalArgumentException {
+        final R r = (R) this.previousMap.get(key);
+        if (null == r)
+            throw Memory.Exceptions.memoryDoesNotExist(key);
+        else
+            return r;
+    }
+
+    @Override
+    public void incr(final String key, final long delta) {
+        checkKeyValue(key, delta);
+        this.currentMap.compute(key, (k, v) -> null == v ? delta : delta + (Long) v);
+    }
+
+    @Override
+    public void and(final String key, final boolean bool) {
+        checkKeyValue(key, bool);
+        this.currentMap.compute(key, (k, v) -> null == v ? bool : bool && (Boolean) v);
+    }
+
+    @Override
+    public void or(final String key, final boolean bool) {
+        checkKeyValue(key, bool);
+        this.currentMap.compute(key, (k, v) -> null == v ? bool : bool || (Boolean) v);
+    }
+
+    @Override
+    public void set(final String key, final Object value) {
+        checkKeyValue(key, value);
+        this.currentMap.put(key, value);
+    }
+
+    @Override
+    public String toString() {
+        return StringFactory.memoryString(this);
+    }
+
+    private void checkKeyValue(final String key, final Object value) {
+        if (!this.memoryKeys.contains(key))
+            throw GraphComputer.Exceptions.providedKeyIsNotAMemoryComputeKey(key);
+        MemoryHelper.validateValue(value);
+    }
 }
