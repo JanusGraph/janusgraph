79c9f9be7c322291614ac929c7304c09ade0d4ee
Stephen Mallette <spmva@genoprime.com>
2015-09-02 15:55:08 -0400

    Bump to TinkerPop 3.0.1-incubating.
    Added GraphProvider.Descriptor (as required for computer test GraphProviders).  Still seeing some failing tests on the groovy side so this branch isn't building yet.

--- titan-cassandra/src/test/java/com/thinkaurelius/titan/blueprints/thrift/ThriftGraphComputerProvider.java
+++ titan-cassandra/src/test/java/com/thinkaurelius/titan/blueprints/thrift/ThriftGraphComputerProvider.java
@@ -4,10 +4,13 @@ import com.thinkaurelius.titan.CassandraStorageSetup;
 import com.thinkaurelius.titan.blueprints.AbstractTitanGraphComputerProvider;
 import com.thinkaurelius.titan.blueprints.AbstractTitanGraphProvider;
 import com.thinkaurelius.titan.diskstorage.configuration.ModifiableConfiguration;
+import com.thinkaurelius.titan.graphdb.olap.computer.FulgoraGraphComputer;
+import org.apache.tinkerpop.gremlin.GraphProvider;
 
 /**
  * @author Matthias Broecheler (me@matthiasb.com)
  */
+@GraphProvider.Descriptor(computer = FulgoraGraphComputer.class)
 public class ThriftGraphComputerProvider extends AbstractTitanGraphComputerProvider {
 
     @Override
