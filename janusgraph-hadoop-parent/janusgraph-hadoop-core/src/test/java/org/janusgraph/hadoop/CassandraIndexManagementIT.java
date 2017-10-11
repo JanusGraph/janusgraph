81e9a7c114145c6db9d1c6bef8a5d601b23722c3
sjudeng <sjudeng@users.noreply.github.com>
2017-10-10 20:29:01 -0500

    Support compression key variants between Cassandra 2.x versus 3.x in tests. Increase timeout in log test to resolve test failure. Disregard value order in multi-property hadoop test. Add scripts to improve automation around Dockerized Cassandra.
    Signed-off-by: sjudeng <sjudeng@users.noreply.github.com>

--- janusgraph-hadoop-parent/janusgraph-hadoop-core/src/test/java/org/janusgraph/hadoop/CassandraIndexManagementIT.java
+++ janusgraph-hadoop-parent/janusgraph-hadoop-core/src/test/java/org/janusgraph/hadoop/CassandraIndexManagementIT.java
@@ -15,15 +15,12 @@
 package org.janusgraph.hadoop;
 
 import org.janusgraph.CassandraStorageSetup;
-import org.janusgraph.diskstorage.configuration.ModifiableConfiguration;
 import org.janusgraph.diskstorage.configuration.WriteConfiguration;
 
 public class CassandraIndexManagementIT extends AbstractIndexManagementIT {
 
     @Override
     public WriteConfiguration getConfiguration() {
-        String className = getClass().getSimpleName();
-        ModifiableConfiguration mc = CassandraStorageSetup.getEmbeddedConfiguration(className);
-        return mc.getConfiguration();
+        return CassandraStorageSetup.getEmbeddedOrThriftConfiguration(getClass().getSimpleName()).getConfiguration();
     }
 }
--- janusgraph-hadoop-parent/janusgraph-hadoop-core/src/test/java/org/janusgraph/hadoop/CassandraInputFormatIT.java
+++ janusgraph-hadoop-parent/janusgraph-hadoop-core/src/test/java/org/janusgraph/hadoop/CassandraInputFormatIT.java
@@ -17,7 +17,6 @@ package org.janusgraph.hadoop;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.janusgraph.CassandraStorageSetup;
-import org.janusgraph.diskstorage.configuration.ModifiableConfiguration;
 import org.janusgraph.diskstorage.configuration.WriteConfiguration;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.structure.util.GraphFactory;
@@ -44,8 +43,6 @@ public class CassandraInputFormatIT extends AbstractInputFormatIT {
 
     @Override
     public WriteConfiguration getConfiguration() {
-        String className = getClass().getSimpleName();
-        ModifiableConfiguration mc = CassandraStorageSetup.getEmbeddedConfiguration(className);
-        return mc.getConfiguration();
+        return CassandraStorageSetup.getEmbeddedOrThriftConfiguration(getClass().getSimpleName()).getConfiguration();
     }
 }
