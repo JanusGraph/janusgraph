088186f8e1ec974db6aa6d8b73c391dd08b2dcd5
Ted Wilmes <twilmes@gmail.com>
2017-12-27 11:58:01 -0600

    Issue #71: Various Gremlin query patterns throw NPE when query.batch=true
    Storage adapter tests were added to exercise all backends in query.batch mode.
    The JanusGraphLocalQueryOptimizerStrategy was updated to disable multiquery on Janus specific steps
    that are nested within RepeatStep, MatchStep, and BranchSteps.

--- janusgraph-core/src/main/java/org/janusgraph/graphdb/tinkerpop/optimize/JanusGraphVertexStep.java
+++ janusgraph-core/src/main/java/org/janusgraph/graphdb/tinkerpop/optimize/JanusGraphVertexStep.java
@@ -81,7 +81,9 @@ public class JanusGraphVertexStep<E extends Element> extends VertexStep<E> imple
         assert !initialized;
         initialized = true;
         if (useMultiQuery) {
-            if (!starts.hasNext()) throw FastNoSuchElementException.instance();
+            if (!starts.hasNext()) {
+                throw FastNoSuchElementException.instance();
+            }
             JanusGraphMultiVertexQuery mquery = JanusGraphTraversalUtil.getTx(traversal).multiQuery();
             List<Traverser.Admin<Vertex>> vertices = new ArrayList<>();
             starts.forEachRemaining(v -> {
