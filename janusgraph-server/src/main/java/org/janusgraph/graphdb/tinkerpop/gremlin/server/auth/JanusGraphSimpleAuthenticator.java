654bdfc861eeffae6f230acb6bbde6abb243565e
Keith Lohnes <lohnesk@gmail.com>
2017-12-28 08:44:49 -0500

    Add JanusGraph specific authentication.
    Issue: #623
    
    This addresses a few concerns.
    
    1) Configure a credential graph that automatically creates the
    appropriate JanusGraph indexes.
    
    2) TinkerPop now supports (as of 3.2.6 and 3.3.0) a combined channelizer
    for enabling port unification for WebSockets and HTTP. This adds the
    necessary classes to enable authentication for one, the other, or both
    with JanusGraph specific authenticators.
    
    3) HTTP Basic Auth is (and should be) slow. This adds an HMAC token
    authentication mechanism to allow for fast authentication over HTTP.
    
    Signed-off-by: Keith Lohnes <lohnesk@gmail.com>

---
+++ janusgraph-server/src/main/java/org/janusgraph/graphdb/tinkerpop/gremlin/server/auth/JanusGraphSimpleAuthenticator.java
@@ -0,0 +1,55 @@
+// Copyright 2017 JanusGraph Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.janusgraph.graphdb.tinkerpop.gremlin.server.auth;
+
+import java.net.InetAddress;
+import java.util.Map;
+
+import org.apache.tinkerpop.gremlin.groovy.jsr223.dsl.credential.CredentialGraph;
+import org.apache.tinkerpop.gremlin.server.auth.AuthenticatedUser;
+import org.apache.tinkerpop.gremlin.server.auth.AuthenticationException;
+import org.apache.tinkerpop.gremlin.server.auth.Authenticator;
+import org.apache.tinkerpop.gremlin.server.auth.SimpleAuthenticator;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+
+/**
+ * A simple implementation of an {@link Authenticator} that uses a {@link Graph} instance as a credential store.
+ * Management of the credential store can be handled through the {@link CredentialGraph} DSL.
+ */
+public class JanusGraphSimpleAuthenticator extends JanusGraphAbstractAuthenticator {
+
+    protected SimpleAuthenticator simpleAuthenticator;
+
+    @Override
+    public void setup(final Map<String, Object> config) {
+        super.setup(config);
+        this.backingGraph.close();
+        simpleAuthenticator = createSimpleAuthenticator();
+        simpleAuthenticator.setup(config);
+    }
+
+    @Override
+    public SaslNegotiator newSaslNegotiator(final InetAddress remoteAddress) {
+        return simpleAuthenticator.newSaslNegotiator(remoteAddress);
+    }
+
+    public AuthenticatedUser authenticate(final Map<String, String> credentials) throws AuthenticationException {
+        return simpleAuthenticator.authenticate(credentials);
+    }
+
+    protected SimpleAuthenticator createSimpleAuthenticator() {
+        return new SimpleAuthenticator();
+    }
+}
---
+++ janusgraph-server/src/main/java/org/janusgraph/graphdb/tinkerpop/gremlin/server/handler/SaslAndHMACAuthenticationHandler.java
@@ -0,0 +1,67 @@
+// Copyright 2017 JanusGraph Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.janusgraph.graphdb.tinkerpop.gremlin.server.handler;
+
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaders.Names.UPGRADE;
+import static org.apache.tinkerpop.gremlin.server.AbstractChannelizer.PIPELINE_AUTHENTICATOR;
+
+import org.apache.tinkerpop.gremlin.server.Settings;
+import org.apache.tinkerpop.gremlin.server.auth.Authenticator;
+import org.apache.tinkerpop.gremlin.server.handler.SaslAuthenticationHandler;
+import org.janusgraph.graphdb.tinkerpop.gremlin.server.auth.HMACAuthenticator;
+import org.janusgraph.graphdb.tinkerpop.gremlin.server.auth.SaslAndHMACAuthenticator;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.HttpMessage;
+
+/**
+ * A class for doing Basic Auth and Token auth using an HMAC as well as
+ * Sasl authentication
+ * @author Keith Lohnes lohnesk@gmail.com
+ */
+@ChannelHandler.Sharable
+public class SaslAndHMACAuthenticationHandler extends SaslAuthenticationHandler {
+
+    private final String HMAC_AUTH = "hmac_authenticator";
+    private HMACAuthenticator hmacAuthenticator;
+
+    public SaslAndHMACAuthenticationHandler(final Authenticator authenticator, final Settings.AuthenticationSettings authenticationSettings) {
+        super(((SaslAndHMACAuthenticator) authenticator).getSimpleAuthenticator(), authenticationSettings);
+        hmacAuthenticator = ((SaslAndHMACAuthenticator) authenticator).getHMACAuthenticator();
+    }
+
+    @Override
+    public void channelRead(final ChannelHandlerContext ctx, final Object obj) throws Exception {
+        if (obj instanceof HttpMessage && !isWebSocket((HttpMessage)obj)) {
+            if (null == ctx.pipeline().get(HMAC_AUTH)) {
+                final HttpHMACAuthenticationHandler authHandler = new HttpHMACAuthenticationHandler(hmacAuthenticator);
+                ctx.pipeline().addAfter(PIPELINE_AUTHENTICATOR, HMAC_AUTH, authHandler);
+            }
+            ctx.fireChannelRead(obj);
+        } else {
+            super.channelRead(ctx, obj);
+        }
+    }
+
+    private boolean isWebSocket(final HttpMessage msg) {
+        final String connectionHeader = msg.headers().get(CONNECTION);
+        final String upgradeHeader = msg.headers().get(UPGRADE);
+        return (null != connectionHeader && connectionHeader.equalsIgnoreCase("Upgrade")) ||
+               (null != upgradeHeader && upgradeHeader.equalsIgnoreCase("WebSocket"));
+    }
+
+}
