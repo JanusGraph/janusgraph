f3fb87dc458047ec57934675890a48a9549fda55
Florian Hockmann <fh@florian-hockmann.de>
2018-06-07 17:41:30 +0200

    Add GraphSON 2 support for JanusGraph predicates #1060
    Signed-off-by: Florian Hockmann <fh@florian-hockmann.de>

--- janusgraph-core/src/main/java/org/janusgraph/graphdb/tinkerpop/JanusGraphPSerializer.java
+++ janusgraph-core/src/main/java/org/janusgraph/graphdb/tinkerpop/JanusGraphPSerializer.java
@@ -14,6 +14,7 @@
 
 package org.janusgraph.graphdb.tinkerpop;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -75,59 +76,61 @@ public class JanusGraphPSerializer extends Serializer<P> {
         }
 
         try {
-
-            if (value instanceof Collection) {
-                switch (predicate) {
-                case "and":
-                    return new AndP((List<P>) value);
-                case "or":
-                    return new OrP((List<P>) value);
-                case "between":
-                    return P.between(((List) value).get(0), ((List) value).get(1));
-                case "inside":
-                    return P.inside(((List) value).get(0), ((List) value).get(1));
-                case "outside":
-                    return P.outside(((List) value).get(0), ((List) value).get(1));
-                case "within":
-                    return P.within((Collection) value);
-                case "without":
-                    return P.without((Collection) value);
-                default:
-                    return (P) P.class.getMethod(predicate, Collection.class).invoke(null, (Collection) value);
-                }
-            } else {
-                switch (predicate) {
-                case "geoIntersect":
-                    return Geo.geoIntersect(value);
-                case "geoDisjoint":
-                    return Geo.geoDisjoint(value);
-                case "geoWithin":
-                    return Geo.geoWithin(value);
-                case "geoContains":
-                    return Geo.geoContains(value);
-                case "textContains":
-                    return Text.textContains(value);
-                case "textContainsFuzzy":
-                    return Text.textContainsFuzzy(value);
-                case "textContainsPrefix":
-                    return Text.textContainsPrefix(value);
-                case "textContainsRegex":
-                    return Text.textContainsRegex(value);
-                case "textFuzzy":
-                    return Text.textFuzzy(value);
-                case "textPrefix":
-                    return Text.textPrefix(value);
-                case "textRegex":
-                    return Text.textRegex(value);
-                default:
-                    return (P) P.class.getMethod(predicate, Object.class).invoke(null, value);
-                }
-            }
-
+            return createPredicateWithValue(predicate, value);
         } catch (final Exception e) {
             log.info("Couldn't deserialize class: " + aClass + ", predicate: " + predicate + ", isCollection: "
                     + isCollection + ",value: " + value, e);
             throw new IllegalStateException(e.getMessage(), e);
+        }
+    }
+
+    public static P createPredicateWithValue(String predicate, Object value) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        if (value instanceof Collection) {
+            switch (predicate) {
+            case "and":
+                return new AndP((List<P>) value);
+            case "or":
+                return new OrP((List<P>) value);
+            case "between":
+                return P.between(((List) value).get(0), ((List) value).get(1));
+            case "inside":
+                return P.inside(((List) value).get(0), ((List) value).get(1));
+            case "outside":
+                return P.outside(((List) value).get(0), ((List) value).get(1));
+            case "within":
+                return P.within((Collection) value);
+            case "without":
+                return P.without((Collection) value);
+            default:
+                return (P) P.class.getMethod(predicate, Collection.class).invoke(null, (Collection) value);
+            }
+        } else {
+            switch (predicate) {
+            case "geoIntersect":
+                return Geo.geoIntersect(value);
+            case "geoDisjoint":
+                return Geo.geoDisjoint(value);
+            case "geoWithin":
+                return Geo.geoWithin(value);
+            case "geoContains":
+                return Geo.geoContains(value);
+            case "textContains":
+                return Text.textContains(value);
+            case "textContainsFuzzy":
+                return Text.textContainsFuzzy(value);
+            case "textContainsPrefix":
+                return Text.textContainsPrefix(value);
+            case "textContainsRegex":
+                return Text.textContainsRegex(value);
+            case "textFuzzy":
+                return Text.textFuzzy(value);
+            case "textPrefix":
+                return Text.textPrefix(value);
+            case "textRegex":
+                return Text.textRegex(value);
+            default:
+                return (P) P.class.getMethod(predicate, Object.class).invoke(null, value);
+            }
         }
     }
 
