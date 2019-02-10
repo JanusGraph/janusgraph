dc109044af82c7f1d9ade4791b74ebe2db2e3e89
Florian Hockmann <fh@florian-hockmann.de>
2019-02-10 17:49:08 +0100

    Stop using deprecated TinkerPop methods #1364
    The following methods were deprecated in TinkerPop 3.3.z and removed in
    TinkerPop 3.4.0:
    
    org.apache.tinkerpop.gremlin.structure.Element.Exceptions#elementAlreadyRemoved(Class, Object)
    org.apache.tinkerpop.gremlin.process.traversal.step.util.HasContainer#makeHasContainers(String, P)

--- janusgraph-core/src/main/java/org/janusgraph/core/InvalidElementException.java
+++ janusgraph-core/src/main/java/org/janusgraph/core/InvalidElementException.java
@@ -53,9 +53,9 @@ public class InvalidElementException extends JanusGraphException {
     }
 
     public static IllegalStateException removedException(JanusGraphElement element) {
-        Class elementClass = Vertex.class.isAssignableFrom(element.getClass())?Vertex.class:
-                (Edge.class.isAssignableFrom(element.getClass())?Edge.class:VertexProperty.class);
-        return Element.Exceptions.elementAlreadyRemoved(elementClass, element.id());
+        Class elementClass = Vertex.class.isAssignableFrom(element.getClass()) ? Vertex.class :
+            (Edge.class.isAssignableFrom(element.getClass()) ? Edge.class : VertexProperty.class);
+        return new IllegalStateException(String.format("%s with id %s was removed.", elementClass.getSimpleName(), element.id()));
     }
 
 }
