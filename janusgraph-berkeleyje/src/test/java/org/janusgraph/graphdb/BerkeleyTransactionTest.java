5309e12afb8e2c6a8a13501069b1af62baa3bbf6
Pavel Ershov <owner.mad.epa@gmail.com>
2019-11-19 18:30:06 +0300

    Add fixes for TP tests on berkeley backend
    Signed-off-by: Pavel Ershov <owner.mad.epa@gmail.com>

--- janusgraph-berkeleyje/src/test/java/org/janusgraph/graphdb/BerkeleyTransactionTest.java
+++ janusgraph-berkeleyje/src/test/java/org/janusgraph/graphdb/BerkeleyTransactionTest.java
@@ -38,13 +38,26 @@ class BerkeleyTransactionTest {
         JanusGraph graph = JanusGraphFactory.open("berkeleyje:" + dir.getAbsolutePath());
 
         GraphTraversalSource traversal = graph.traversal();
-        for (int i = 0; i < 15000; i++) {
+        for (int i = 0; i < 10; i++) {
             traversal.addV().property("a", "2").next();
         }
         traversal.tx().commit();
 
         GraphTraversalSource g = graph.tx().createThreadedTx().traversal();
-        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> g.V().has("a", "2").toList());
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+            g.V()
+             .has("a", "2")
+             .sideEffect(ignored -> {
+                 try {
+                     // artificially slow down the traversal execution so that
+                     // this test has a chance to interrupt it before it finishes
+                     Thread.sleep(100);
+                 } catch (InterruptedException e) {
+                     Thread.currentThread().interrupt();
+                 }
+             })
+             .toList();
+        });
         Thread.sleep(100);
         g.tx().rollback();
         graph.close();
