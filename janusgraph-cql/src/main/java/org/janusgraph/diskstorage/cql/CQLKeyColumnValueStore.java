2f7fce3a2893ef6f68e1f49dd38018ce16f3ec5d
Ganesh Guttikonda <gguttikonda@snapfish-llc.com>
2020-01-03 11:38:13 -0800

    * Remove unused import
    Signed-off-by: Ganesh Guttikonda <gguttikonda@snapfish-llc.com>

--- janusgraph-cql/src/main/java/org/janusgraph/diskstorage/cql/CQLKeyColumnValueStore.java
+++ janusgraph-cql/src/main/java/org/janusgraph/diskstorage/cql/CQLKeyColumnValueStore.java
@@ -51,7 +51,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
