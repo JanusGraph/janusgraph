1154d890232b0eaef24be9d48854bb5c9dc69d24
Boxuan Li <liboxuan@connect.hku.hk>
2021-11-24 19:35:51 -0500

    Move GraphCentricQueryBenchmark to janusgraph-benchmark module
    Signed-off-by: Boxuan Li <liboxuan@connect.hku.hk>

--- janusgraph-benchmark/src/main/java/org/janusgraph/GraphCentricQueryBenchmark.java
+++ janusgraph-benchmark/src/main/java/org/janusgraph/GraphCentricQueryBenchmark.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.janusgraph.graphdb.query;
+package org.janusgraph;
 
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.janusgraph.core.Cardinality;
