7adbba6249476c534699f5c00507a67d889e6ea0
Boxuan Li <liboxuan@connect.hku.hk>
2022-05-12 20:31:06 -0400

    Report benchmark performance per Pull Request
    This commit adds github-action-benchmark which
    can report performance change between current
    commit and previous commit.
    
    Signed-off-by: Boxuan Li <liboxuan@connect.hku.hk>

--- janusgraph-benchmark/src/main/java/org/janusgraph/BenchmarkRunner.java
+++ janusgraph-benchmark/src/main/java/org/janusgraph/BenchmarkRunner.java
@@ -14,14 +14,27 @@
 
 package org.janusgraph;
 
+import org.apache.tinkerpop.shaded.jackson.databind.ObjectMapper;
+import org.openjdk.jmh.results.Result;
+import org.openjdk.jmh.results.RunResult;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.runner.options.TimeValue;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class BenchmarkRunner {
-    public static void main(String[] args) throws RunnerException {
+    public static void main(String[] args) throws RunnerException, IOException {
         final ChainedOptionsBuilder builder = new OptionsBuilder()
             .forks(1)
             .measurementTime(TimeValue.seconds(5))
@@ -36,6 +49,34 @@ public class BenchmarkRunner {
             builder.include(".*Benchmark");
         }
         builder.exclude(".*StaticArrayEntryListBenchmark");
-        new Runner(builder.build()).run();
+        Collection<RunResult> results = new Runner(builder.build()).run();
+        Map<String, Double> metrics = new HashMap<>();
+        for (RunResult result : results) {
+            Result primaryResult = result.getPrimaryResult();
+            String benchmark = result.getParams().getBenchmark();
+            double score = primaryResult.getScore();
+            String unit = primaryResult.getScoreUnit();
+            if (!"ms/op".equals(unit)) {
+                throw new IllegalArgumentException("Please use ms/op as measurement for benchmark " + benchmark);
+            }
+            // we sum up results with different params against same benchmark
+            metrics.put(benchmark, metrics.getOrDefault(benchmark, 0d) + score);
+        }
+        List<Map<String, Object>> outputs = new ArrayList<>();
+        for (Map.Entry<String, Double> entry : metrics.entrySet()) {
+            outputs.add(new HashMap() {{
+                put("name", entry.getKey());
+                put("value", entry.getValue());
+                put("unit", "ms/op");
+            }});
+        }
+        String workspace = System.getenv("${GITHUB_WORKSPACE}");
+        File file = new File(workspace, "benchmark.json");
+        System.out.println("writing to " + file.getAbsolutePath());
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(outputs);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(json);
+        writer.close();
     }
 }
--- janusgraph-benchmark/src/main/java/org/janusgraph/JanusGraphSpeedBenchmark.java
+++ janusgraph-benchmark/src/main/java/org/janusgraph/JanusGraphSpeedBenchmark.java
@@ -25,13 +25,20 @@ import org.janusgraph.core.schema.JanusGraphManagement;
 import org.janusgraph.diskstorage.configuration.ModifiableConfiguration;
 import org.janusgraph.graphdb.configuration.GraphDatabaseConfiguration;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
 
+import java.util.concurrent.TimeUnit;
+
 @State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class JanusGraphSpeedBenchmark {
 
     @Param({ "1000", "10000", "100000" })
