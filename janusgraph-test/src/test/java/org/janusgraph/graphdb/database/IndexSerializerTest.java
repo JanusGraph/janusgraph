e96f731c6f9ed71e3ff72f1f9e3997b5523db70a
Jan Jansen <farodin91@users.noreply.github.com>
2022-09-29 13:16:15 +0200

    Fix test error with mockito (#3231)
    * Fix test error with mockito
    
    Signed-off-by: Jan Jansen <jan.jansen@gdata.de>
    Co-authored-by: rngcntr <7890887+rngcntr@users.noreply.github.com>

--- janusgraph-test/src/test/java/org/janusgraph/graphdb/database/IndexSerializerTest.java
+++ janusgraph-test/src/test/java/org/janusgraph/graphdb/database/IndexSerializerTest.java
@@ -125,6 +125,7 @@ public class IndexSerializerTest {
 
     private JanusGraphElement mockIndexableElement(String key, String value, boolean indexable) {
         StandardJanusGraphTx tx = mock(StandardJanusGraphTx.class);
+        doReturn(tx).when(tx).getNextTx();
         JanusGraphElement indexableElement = spy(new StandardVertex(tx, 1L, ElementLifeCycle.New));
         Property pk2 = indexableElement.property(key, value);
         Iterator it = Arrays.asList(pk2).iterator();
